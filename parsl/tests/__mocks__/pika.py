3e6741e73c2e5e640a8a8623fa2f07724c828c8c
Kelly L. Rowland <kellylynnerowland@gmail.com>
2020-09-10 14:40:21 -0700

    mock pika fxns to test send

---
+++ parsl/tests/__mocks__/pika.py
@@ -0,0 +1,15 @@
+class Channel:
+    def __init__(self): pass
+    def exchange_declare(self): pass
+    def queue_declare(self, queue): pass
+    def queue_bind(self): pass
+    def basic_qos(self): pass
+    def basic_consume(self): pass
+    def start_consuming(self): pass
+    def basic_publish(self, exchange, routing_key, body): pass
+    def basic_ack(self): pass
+
+class Connection:
+    def __init__(self): pass
+    def channel(self): return Channel()
+
--- parsl/tests/test_send.py
+++ parsl/tests/test_send.py
@@ -1,6 +1,8 @@
 import pytest
 import jaws_parsl.send
 from click.testing import CliRunner
+from unittest.mock import Mock
+from tests.__mocks__ import pika
 
 @pytest.fixture
 def config_file(tmp_path):
@@ -12,9 +14,11 @@ def config_file(tmp_path):
     cfg.write_text(content)
     return cfg.as_posix()
 
-
-def test_send(config_file):
+def test_send(config_file, monkeypatch):
+    mocked_pika = Mock()
+    mocked_pika.BlockingConnection.return_value = pika.Connection()
+    monkeypatch.setattr('jaws_parsl.send.pika', mocked_pika)
     runner = CliRunner()
     result = runner.invoke(jaws_parsl.send.send, ['-cl', 'cori', '-cmd', config_file])
-    assert result.exit_code == 1 # should fail w/connection error right now
+    assert result.exit_code == 0
 
