448d7fe5df3530cd69670d7881e97a43f5f3eeac
Edward Steven Kirton <eskirton@lbl.gov>
2020-09-12 01:01:51 -0700

    add subprocess.run

--- grid/jaws_grid/GridFactory.py
+++ grid/jaws_grid/GridFactory.py
@@ -1,40 +1,52 @@
+import subprocess
+
 class Slurm(object):
     def submit(self, params):
         """
         sbatch
         """
-        options=["sbatch"]
+        cmd=["sbatch"]
         if "account" in params:
-            options.append(f"-A {params['account']}")
+            cmd.extend(["-A", params['account']])
         if "constraint" in params:
-            options.append(f"-C {params['constraint']}")
+            cmd.extend(["-C", params['constraint']])
         if "cpu" in params:
-            options.append(f"-c {params['cpu']}")
+            cmd.extend(["-c", params['cpu']])
         if "exclusive" in params:
-            options.append("--exclusive")
+            cmd.append("--exclusive")
         if "name" in params:
-            options.append(f"-N {params['name']}")
+            cmd.extend(["-N", params['name']])
         if "memory" in params:
-            options.append(f"--mem={params['memory']}")
+            cmd.append(f"--mem={params['memory']}")
         elif "mem-per-cpu" in params:
-            options.append(f"--mem-per-cpu={params['mem-per-cpu']}")
+            cmd.append(f"--mem-per-cpu={params['mem-per-cpu']}")
         if "output" in params:
-            options.append(f"-o {params['output']}")
+            cmd.extend(["-o", params['output']])
         if "error" in params:
-            options.append(f"-e {params['error']}")
+            cmd.extend(["-e", params['error']])
         if "qos" in params:
-            options.append(f"-q {params['qos']}")
+            cmd.extend(["-q", params['qos']])
         if "time" in params:
-            options.append(f"-t {params['time']}")
-        cmd=' '.join(options)
-        # TODO
+            cmd.extend(["-t", params['time']])
+        try:
+            result = subprocess.run(cmd, capture_output=True, text=True, check=True)
+        except FileNotFoundError as error:
+            print(f"FileNotFound: {error}")
+        except subprocess.CalledProcessError as error:
+            print(f"CalledProcessError: {error}")
+        else:
+            print(f"rc={result.returncode}")
+            print(f"stdout:\n{result.stdout}")
+            print(f"stderr:\n{result.stderr}")
 
     def cancel(self, job_id):
         """
         scancel
         """
-        cmd=f"scancel {job_id}" 
-        # TODO
+        cmd=["scancel", str(job_id)]
+        result = subprocess.run(cmd, capture_output=True, text=True)
+        print(result.stdout)
+        print(result.stderr)
 
     def queue(self, job_id):
         """
--- worker/jaws_worker/worker.py
+++ worker/jaws_worker/worker.py
@@ -1,17 +1,18 @@
-def kill(job_id):
-    job_id=params['job_id']
-    pid=lookup_pid(job_id)
+import subprocess
+import psutil
 
-    list_descendants ()
-    {
-      local children=$(ps -o pid= --ppid "$1")
+def run(cmd, max_min=):
+    try:
+        proc = subprocess.run(cmd.split(), timeout=max_sec)
+    except subprocess.TimeoutExpired as error:
+        print(f"Maximum time exceeded.")
+        kill(pid)
 
-      for pid in $children
-      do
-        list_descendants "$pid"
-      done
+def kill(parent_pid):
+    parent = psutil.Process(parent_pid)
+    for child in parent.children(recursive=True):  # or parent.children() for recursive=False
+        child.kill()
+    parent.kill()
 
-      echo "$children"
-    }
+    # WRITE "rc" FILE SO CROMWELL DETECTS FAILED TASK
 
-    kill $(list_descendants $$)
