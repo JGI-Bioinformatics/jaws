18ad82a664bdec17f04b1d38a6ff7ad139646172
Edward Steven Kirton <eskirton@lbl.gov>
2020-09-12 04:58:49 -0700

    add worker rpc functions

--- worker/jaws_worker/rpc_operations.py
+++ worker/jaws_worker/rpc_operations.py
@@ -1,10 +1,16 @@
 """
-jaws-worker RPC operations
+jaws-worker RPC operations.
+
+There must be at least two RPC server threads: one to run a task and one to receive calls while
+executing that task (e.g. "cancel" command).
 """
 
-import logging
-import sqlalchemy.exc
-from datetime import datetime
+#import logging
+import subprocess
+import psutil
+import os
+import os.path
+import signal
 from jaws_worker import config
 from jaws_worker.database import Session
 from jaws_worker.models import Jobs, Job_Log
@@ -15,9 +21,9 @@ from jaws_rpc.response import success, failure
 logger = logging.getLogger(__package__)
 
 
-def health(params):
+def worker_status(params):
     """
-    Worker health check.
+    Worker health check.  Simply returns success if alive..
 
     :param params: message from jaws-site
     :type params: dict
@@ -25,58 +31,126 @@ def health(params):
     :rtype: dict
     """
     return success()
+     
 
-def job_status(params):
+def worker_quit(params):
     """
-    Check a job's status.
-
-    :param params: message from jaws-site
-    :type params: dict
-    :return: JSON-RPC2 response
-    :rtype: dict
+    Worker should shut down.
     """
-    job_id = params["job_id"]
-    # TODO
-    result = True
-    return success(result)
+    quit = threading.Thread(target=__quit)
+    quit.start()
+    return success()
 
 
-def run_job(params):
+def __quit:
+    """Wait a moment in order to return RPC response and terminate all threads."""
+    sleep(2)
+    pid = os.getpid()
+    os.kill(pid, signal.STOP)
+
+
+def run_task(params):
     """
-    Run a job.
+    Run a task.
 
     :params param: message from jaws-site
     :type params: dict
     :return: JSON-RPC2 response
     :rtype: dict
     """
-    # TODO
-    return success()
+    # init vars
+    cmd=params["cmd"].split()
+    call_root_dir=params["call_root"]
+    rc_file=os.path.join(call_root, "execution", "rc")
+    max_time = params["time"].split(":")  # format is "hh:mm:ss"
+    for n in range(len(max_time), 3):
+        max_time.insert(0,0)  # add any missing fields
+    max_sec=max_time[0]*3600+max_time[1]*60+max_time[2]
+
+    # Run the Task in the background.
+    # The worker doesn't care about the Task's exit status or output.
+    # jaws-site will know when the Task is complete and won't send the
+    # worker another Task before then (or will instruct worker to quit).
+    try:
+        proc = subprocess.Popen(cmd)
+    except FileNotFound as error:
+        return failure(404, f"FileNotFound: {error}")
+
+    # return pid
+    result = {"pid": proc.pid}
+    return success(result)
 
 
-def cancel_job(params):
+def task_status(params):
     """
-    Abort a job.
+    Check if Task is running.
+    If it's not running, check if 'rc' file exists.
+    If so, read rc, else write rc of 1.
     """
-    # TODO
-    return success()
+    task_pid = params["task_id"]
+    is_alive = True
+    rc = None
+    try:
+        task = psutil.Process(task_pid) 
+    except psutil.NoSuchProcess:
+        # not running
+        is_alive = False
+        # check if rc file exists
+        if os.path.isfile(rc_file):
+            with open(rc_file, 'r') as fh:
+                rc=fh.read()
+        else:
+            rc="1"
+            with open(rc_file, 'w') as fh:
+                fh.write(rc)
+    result = {
+        "is_alive": is_alive,
+        "rc": rc
+    }
+    return success(result)
 
 
-def quit(params):
+def kill_task(params):
     """
-    Worker should shut down.
+    Recursively kill process and write return code to file.
+    Cromwell reads the rc_file to determine status.
     """
+    task_pid=params["task_id"]
+    call_root_dir=params["call_root"]
+    rc_file=os.path.join(call_root, "execution", "rc")
+    #rc_file=params["rc_file"]
+    rc=9
+    if "rc" in params:
+        rc=params["rc"]
+
+    try:
+        task = psutil.Process(task_pid)
+    except psutil.NoSuchProcess:
+        # not running; write rc file if it doesn't exist
+        if not os.path.isfile(rc_file):
+            with open(rc_file, 'w') as fh:
+                fh.write(str(rc))
+        return success()
+
+    # recursively kill
+    for child in task.children(recursive=True):
+        child.kill()
+    task.kill()
+
+    # write rc file
+    with open(rc_file, 'w') as fh:
+        fh.write(str(rc))
     return success()
 
 
 # THIS DISPATCH TABLE IS USED BY jaws_rpc.rpc_server AND REFERENCES FUNCTIONS ABOVE
 operations = {
-    "health": {"function": health, "required_params": []},
-    "job_status": {"function": job_status, "required_params": ["job_id"]},
-    "run_job": {
-        "function": run_job,
-        "required_params": ["run_id", "cromwell_run_id", "job_id", "task_name", "attempt", "script", "num_cores", "ram_gb", "minutes"]
+    "worker_status": {"function": worker_status, "required_params": []},
+    "worker_quit": {"function": worker_quit, "required_params": ["task_id"]},
+    "run_task": {
+        "function": run_task,
+        "required_params": ["run_id", "cromwell_run_id", "task_id", "task_name", "attempt", "script", "num_cores", "ram_gb", "minutes"]
     },
-    "cancel_job": {"function": cancel_job, "required_params": ["job_id"]},
-    "quit": {"function": quit, "required_params": ["job_id"]},
+    "task_status": {"function": task_status, "required_params": ["task_id"]},
+    "kill_task": {"function": kill_task, "required_params": ["task_id"]},
 }
