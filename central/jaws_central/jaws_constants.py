ac1393b5400aff1f2414c00d94df963b3b8421f4
Edward Steven Kirton <eskirton@lbl.gov>
2020-09-12 22:41:46 -0700

    remove JTM-specific task states

--- central/jaws_central/jaws_constants.py
+++ central/jaws_central/jaws_constants.py
@@ -20,15 +20,10 @@ run_status_msg = {
 }
 
 task_status_msg = {
-    "ready": "The job has been prepared by Cromwell and submitted to JTM",
-    "queued": "The job was received by JTM-manager and sent to JTM-worker",
-    "pending": "The job was receive by JTM-worker and is awaiting resources",
-    "running": "The job is currently executing",
+    "created": "The task has been prepared by Cromwell",
+    "queued": "The task is waiting to be assigned to a worker",
+    "running": "The job is currently being executed",
     "success": "The job completed successfully",
     "failed": "The job has failed",
-    "outofresource": "The job exceeeded the reserved RAM; increase the amount in the WDL and rerun",
-    "terminated": "The run was terminated",
-    "invalidtask": "The task definition in the message from jtm_submit is not valid",
-    "timeout": "The worker timed out",
-    "lostconnection": "The manager lost connection with the worker",
+    "aborted": "The task was canceled",
 }
