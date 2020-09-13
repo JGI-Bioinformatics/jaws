c252556cfe6104052d8e568f1966f2f3e1a962a5
Edward Steven Kirton <eskirton@lbl.gov>
2020-09-12 17:56:40 -0700

    site_rpc_operations now uses Task object

--- site/jaws_site/site_rpc_operations.py
+++ site/jaws_site/site_rpc_operations.py
@@ -1,57 +1,76 @@
 """
 jaws-site has two RPC servers, one on the Central RMQ server and one on the Site RMQ server.
+The latter may be on localhost and not accessible from outside.
+
 This package provides the RPC operations for the Site's operations, namely receiving
-instructions from Cromwell.
+instructions from Cromwell via jaws-backend.
 """
 
-import logging
-import sqlalchemy.exc
-from datetime import datetime
+# import logging
 from jaws_site import config
-from jaws_site.database import Session
-from jaws_site.models import Job_Log
 from jaws_rpc.responses import success, failure
+from jaws_site.Task import Task
 
 
 # config and logging must be initialized before importing this module
-logger = logging.getLogger(__package__)
+# logger = logging.getLogger(__package__)
 
 
 def submit(params):
     """
-    Receive a new task submission request (from Cromwell).
-    The job is saved in the database and the job_id is returned.
+    Receive a new task submission request.
+    The job is saved in the database and the task_id is returned.
+    The task_daemon is responsible for actually sending the task to a worker.
     """
-    # insert into db, get task_id
-    task_id = None  # TODO
-    # submit to worker via rpc
-    # TODO
-    result = {"job_id": job_id}
-    return success(result)
+    try:
+        task = Task.new(config.conf, params)
+    except Exception as error:
+        return failure(500, f"Failed to submit task: {error}")
+    return success({"task_id": task.id})
+
 
 def kill(params):
     """
     Receive an abort task command (from Cromwell).
     """
-    # TODO
+    task = Task.get(config.conf, params["task_id"])
+    try:
+        task.kill()
+    except Exception as error:
+        return failure(500, f"Failed to kill task: {error}")
     return success()
 
 
 def check_alive(params):
     """
-    Job status check query (from Cromwell).
-    The status is retrieved from the db; the worker is not queried.
+    Check if a Task is running.
     Unless polling is on, Cromwell only calls this after a restart.
     """
-    # TODO
-    is_alive = True
-    result = {"is_alive": is_alive}
-    return success(result)
+    task = Task.get(config.conf, params["task_id"])
+    try:
+        status = task.status()
+    except Exception as error:
+        return failure(500, f"Failed to get task status: {error}")
+    if status in ("queued", "running"):
+        return success(True)
+    else:
+        return success(False)
 
 
 # THIS DISPATCH TABLE IS USED BY jaws_rpc.rpc_server AND REFERENCES FUNCTIONS ABOVE
 operations = {
-    "submit": {"function": submit, "required_params": []},
-    "kill": {"function": kill, "required_params": []},
-    "check_alive": {"function": check_alive, "required_params": []},
+    "submit": {
+        "function": submit,
+        "required_params": [
+            "script",
+            "job_name",
+            "cwd",
+            "out",
+            "err",
+            "max_time",
+            "memory_gb",
+        ],
+    },
+    "kill": {"function": kill, "required_params": ["task_id"]},
+    "check_alive": {"function": check_alive, "required_params": ["task_id"]},
 }
