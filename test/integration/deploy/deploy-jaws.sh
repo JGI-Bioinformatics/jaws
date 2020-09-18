e25974be8d6c9fb56f9c994f1789c006501e2419
Stephan Trong <strong@lbl.gov>
2020-09-18 09:42:13 -0700

    in progress

--- test/integration/deploy/deploy-jaws.sh
+++ test/integration/deploy/deploy-jaws.sh
@@ -12,12 +12,11 @@ SITE_NAME=$1
 DEPLOY_NAME=$2
 INSTALL_DIR=$(readlink -f $3)
 DEPLOY_DIR="$INSTALL_DIR/deploy"
-SRC_DIR=$(pwd)
+SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 export SITE_NAME=$SITE_NAME
 export DEPLOY_NAME=$DEPLOY_NAME
 export DEPLOY_DIR=$DEPLOY_DIR
-export SRC_DIR=$SRC_DIR
 
 test -d "$INSTALL_DIR" && rm -rf "$INSTALL_DIR"
 mkdir "$INSTALL_DIR"
@@ -31,9 +30,9 @@ python3 -m venv $DEPLOY_DIR/venv && \
   pip install jinja2 wheel
 
 if [[ -z "$4" ]];then
-    cmd="./test/integration/deploy/setup_jaws $SITE_NAME $DEPLOY_NAME $INSTALL_DIR"
+    cmd="$SRC_DIR/setup_jaws $SITE_NAME $DEPLOY_NAME $INSTALL_DIR"
 else
-    cmd="./test/integration/deploy/setup_jaws $SITE_NAME $DEPLOY_NAME $INSTALL_DIR -c $4"
+    cmd="$SRC_DIR/setup_jaws $SITE_NAME $DEPLOY_NAME $INSTALL_DIR -c $4"
 fi
 echo
 echo "Setting up deployment environment ..."
