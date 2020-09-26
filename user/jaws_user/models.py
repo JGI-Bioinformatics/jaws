40e3e8d08e215fa167612b950936c176eadc6de9
Edward Steven Kirton <eskirton@lbl.gov>
2020-09-25 22:12:53 -0700

    remove auth token from users table

--- user/jaws_user/models.py
+++ user/jaws_user/models.py
@@ -1,16 +1,6 @@
 """Sqlalchemy ORM models for jaws-user database"""
 
-import datetime
-from sqlalchemy import (
-    Column,
-    DateTime,
-    String,
-    Integer,
-    Boolean,
-    ForeignKey,
-    Text,
-    UniqueConstraint,
-)
+from sqlalchemy import Column, String, Boolean
 from jaws_user.database import Base
 
 
@@ -22,7 +12,6 @@ class User(Base):
     email = Column(String(64), nullable=False)
     name = Column(String(64), nullable=True)
     is_admin = Column(Boolean, nullable=False, default=False)
-    jaws_token = Column(String(256), nullable=False)
     globus_id = Column(String(36), nullable=True)
     auth_refresh_token = Column(String(256), nullable=True)
     transfer_refresh_token = Column(String(256), nullable=True)
