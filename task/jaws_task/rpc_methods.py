a30c65be23a25158ae600a13f3e63a2ae8dbaf40
Edward Steven Kirton <eskirton@lbl.gov>
2020-09-29 21:41:24 -0700

    mv dict from jaws_constants.py to rpc_methods.py

--- task/jaws_task/rpc_methods.py
+++ task/jaws_task/rpc_methods.py
@@ -20,6 +20,21 @@ from jaws_rpc.responses import success, failure
 cromwell = Cromwell(config.conf.get("CROMWELL", "url"))
 logger = logging.getLogger(__package__)
 
+# constants
+task_status_msg = {
+    "ready": "The job has been prepared by Cromwell and submitted to JTM",
+    "queued": "The job was received by JTM-manager and sent to JTM-worker",
+    "pending": "The job was receive by JTM-worker and is awaiting resources",
+    "running": "The job is currently executing",
+    "success": "The job completed successfully",
+    "failed": "The job has failed",
+    "outofresource": "The job exceeeded the reserved RAM; increase the amount in the WDL and rerun",
+    "terminated": "The run was terminated",
+    "invalidtask": "The task definition in the message from jtm_submit is not valid",
+    "timeout": "The worker timed out",
+    "lostconnection": "The manager lost connection with the worker",
+}
+
 
 def get_task_log(params):
     """Retrieve task log from database"""
