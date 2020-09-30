361f4b8a302a8ac33f80972bdec0933a691cee3e
Edward Steven Kirton <eskirton@lbl.gov>
2020-09-29 23:03:08 -0700

    separate api from rpc method

---
+++ user/jaws_user/api.py
@@ -0,0 +1,97 @@
+"""
+User service API
+"""
+
+import logging
+import globus_sdk
+from sqlalchemy.exc import SQLAlchemyError
+from jaws_user import config
+from jaws_user.db import Session, User
+
+logger = logging.getLogger(__package__)
+
+
+class DatabaseError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+def get_user(params):
+    """
+    Retrieve a user's records by ID.
+
+    :param params: contains "user_id"
+    :type params: dict
+    :return: valid JSON-RPC2 response
+    :rtype: dict
+    """
+    user_id = params["user_id"]
+    logger.debug(f"Get user {user_id}")
+    session = Session()
+    try:
+        user = session.query(User).get(user_id)
+    except SQLAlchemyError as error:
+        raise DatabaseError(f"{error}")
+    if user is None:
+        return None
+    result = {
+        "user_id": user.id,
+        "email": user.email,
+        "name": user.name,
+        "is_admin": user.admin,
+        "globus_id": user.globus_id,
+        "auth_refresh_token": user.auth_refresh_token,
+        "transfer_refresh_token": user.transfer_refresh_token,
+    }
+    session.close()
+    return result
+
+
+def update_user(user_id, auth_refresh_token, transfer_refresh_token):
+    """Save a user's Globus tokens.  Only the refresh tokens are required with the RefreshTokenAuthorizer
+    and the refresh tokens never expire.  This is used when new users grant the jaws app globus access.
+
+    :param user_id: user ID
+    :type user_id: str
+    :param auth_refresh_token: Auth Refresh Token
+    :type auth_refresh_token: str
+    :param auth_refresh_token: Transfer Refresh Token
+    :type transfer_refresh_token: str
+    :return:
+    """
+    # GET USER INFO FROM GLOBUS
+    client = globus_sdk.NativeAppAuthClient(config.conf.get("GLOBUS", "client_id"))
+    authorizer = globus_sdk.RefreshTokenAuthorizer(auth_refresh_token, client)
+    auth_client = globus_sdk.AuthClient(authorizer=authorizer)
+    user_info = auth_client.oauth2_userinfo()
+    globus_id = user_info["sub"]
+    name = user_info["name"]
+    email = user_info["email"]
+
+    # INSERT OR UPDATE
+    session = Session()
+    try:
+        user_rec = session.query(User).get(user_id)
+    except SQLAlchemyError as error:
+        raise DatabaseError(f"{error}")
+    if user_rec is None:
+        # insert new
+        new_user = User(
+            name=name,
+            email=email,
+            globus_id=globus_id,
+            auth_refresh_token=auth_refresh_token,
+            transfer_refresh_token=transfer_refresh_token,
+        )
+        session.add(new_user)
+    else:
+        # update existing
+        user_rec.name = name
+        user_rec.globus_id = globus_id
+        user_rec.auth_refresh_token = auth_refresh_token
+        user_rec.transfer_refresh_token = transfer_refresh_token
+    try:
+        session.commit()
+    except Exception as error:
+        session.rollback()
+        raise DatabaseError(f"{error}")
--- user/jaws_user/rpc_methods.py
+++ user/jaws_user/rpc_methods.py
@@ -1,13 +1,10 @@
 """
-RPC operations for jaws-user service
+JAWS User Service RPC methods
 """
 
 import logging
-import globus_sdk
-from sqlalchemy.exc import SQLAlchemyError
 from jaws_rpc.responses import success, failure
-from jaws_user import config
-from jaws_user.db import Session, User
+from jaws_user import api
 
 logger = logging.getLogger(__package__)
 
@@ -23,31 +20,21 @@ def get_user(params):
     """
     user_id = params["user_id"]
     logger.debug(f"Get user {user_id}")
-    session = Session()
     try:
-        user = session.query(User).get(user_id)
-    except SQLAlchemyError as e:
-        return failure(500, f"Db error: {e}")
+        user = api.get_user(user_id)
+    except Exception as error:
+        return failure(500, f"User service error: {error}")
     if user is None:
         err = f"User {user_id} not found"
         logger.error(err)
         return failure(404, err)
-    result = {
-        "user_id": user.id,
-        "email": user.email,
-        "name": user.name,
-        "is_admin": user.admin,
-        "globus_id": user.globus_id,
-        "auth_refresh_token": user.auth_refresh_token,
-        "transfer_refresh_token": user.transfer_refresh_token,
-    }
-    session.close()
-    return success(result)
+    return success(user)
 
 
 def update_user(user_id, auth_refresh_token, transfer_refresh_token):
     """Save a user's Globus tokens.  Only the refresh tokens are required with the RefreshTokenAuthorizer
     and the refresh tokens never expire.  This is used when new users grant the jaws app globus access.
+    If the user record exists, it will be updated, otherwise a new record shall be inserted.
 
     :param user_id: user ID
     :type user_id: str
@@ -57,38 +44,10 @@ def update_user(user_id, auth_refresh_token, transfer_refresh_token):
     :type transfer_refresh_token: str
     :return:
     """
-    # GET USER INFO FROM GLOBUS
-    client = globus_sdk.NativeAppAuthClient(config.conf.get("GLOBUS", "client_id"))
-    authorizer = globus_sdk.RefreshTokenAuthorizer(auth_refresh_token, client)
-    auth_client = globus_sdk.AuthClient(authorizer=authorizer)
-    user_info = auth_client.oauth2_userinfo()
-    globus_id = user_info["sub"]
-    name = user_info["name"]
-    email = user_info["email"]
-
-    # INSERT OR UPDATE
-    session = Session()
     try:
-        user_rec = session.query(User).get(user_id)
-    except SQLAlchemyError as e:
-        return (500, f"Db error: {e}")
-    if user_rec is None:
-        # insert new
-        new_user = User(
-            name=name,
-            email=email,
-            globus_id=globus_id,
-            auth_refresh_token=auth_refresh_token,
-            transfer_refresh_token=transfer_refresh_token,
-        )
-        session.add(new_user)
-    else:
-        # update existing
-        user_rec.name = name
-        user_rec.globus_id = globus_id
-        user_rec.auth_refresh_token = auth_refresh_token
-        user_rec.transfer_refresh_token = transfer_refresh_token
-    session.commit()
+        api.update_user(user_id, auth_refresh_token, transfer_refresh_token)
+    except Exception as error:
+        return failure(500, f"User service error: {error}")
     return success()
 
 
