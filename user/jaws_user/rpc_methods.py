eefb9134482d60dd9deea89cb5c254cb9fd2e0f3
Edward Steven Kirton <eskirton@lbl.gov>
2020-09-30 22:12:45 -0700

    edit rpc methods docstrings

--- user/jaws_user/rpc_methods.py
+++ user/jaws_user/rpc_methods.py
@@ -11,12 +11,10 @@ logger = logging.getLogger(__package__)
 
 def get_user(params):
     """
-    Retrieve a user's records by ID.
+    Retrieve a user's record.
 
-    :param params: contains "user_id"
-    :type params: dict
-    :return: valid JSON-RPC2 response
-    :rtype: dict
+    Required parameters: user_id
+    Returns: dict of user information
     """
     user_id = params["user_id"]
     logger.debug(f"Get user {user_id}")
@@ -32,18 +30,13 @@ def get_user(params):
 
 
 def update_user(user_id, auth_refresh_token, transfer_refresh_token):
-    """Save a user's Globus tokens.  Only the refresh tokens are required with the RefreshTokenAuthorizer
-    and the refresh tokens never expire.  This is used when new users grant the jaws app globus access.
-    If the user record exists, it will be updated, otherwise a new record shall be inserted.
-
-    :param user_id: user ID
-    :type user_id: str
-    :param auth_refresh_token: Auth Refresh Token
-    :type auth_refresh_token: str
-    :param auth_refresh_token: Transfer Refresh Token
-    :type transfer_refresh_token: str
-    :return:
     """
+    Add a new user or update a user's Globus tokens (upsert).
+
+    Required parameters: user_id, auth_refresh_token, transfer_refresh_token
+    Return: None
+    """
+    # Only the refresh tokens are required with the RefreshTokenAuthorizer and the refresh tokens never expire.
     try:
         api.update_user(user_id, auth_refresh_token, transfer_refresh_token)
     except api.DatabaseError as error:
