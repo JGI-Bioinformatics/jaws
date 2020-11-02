fe505a0ee31f56c168781452d47f1e07e228bbe2
Kelly Rowland <kellylynnerowland@gmail.com>
2020-11-02 11:53:58 -0800

    add cori parsl config file

---
+++ parsl/jaws_parsl/cori.py
@@ -0,0 +1,42 @@
+from parsl.config import Config
+from parsl.providers import SlurmProvider
+from parsl.launchers import SrunLauncher
+from parsl.executors import HighThroughputExecutor
+from parsl.addresses import address_by_interface
+
+cori_sched_opts = '#SBATCH -C haswell'
+cori_sched_opts += '\n#SBATCH -A m342'
+
+config = Config(
+    executors=[
+        HighThroughputExecutor(
+            label='cori',
+            # This is the network interface on the login node to
+            # which compute nodes can communicate
+            address=address_by_interface('bond0.144'),
+            cores_per_worker=2,
+            provider=SlurmProvider(
+                # 'regular',  # Partition / QOS
+                'debug',  # Partition / QOS
+                nodes_per_block=2,
+                init_blocks=1,
+                # string to prepend to #SBATCH blocks in the submit
+                # script to the scheduler eg: '#SBATCH --constraint=knl,quad,cache'
+                scheduler_options=cori_sched_opts,
+
+                # *** NOTE *** worker_init must be setup 
+                # Command to be run before starting a worker, such as:
+                # 'module load Anaconda; source activate parsl_env'.
+                worker_init='module load python; source activate parsl-env',
+                # We request all hyperthreads on a node.
+                
+                launcher=SrunLauncher(),
+                
+                walltime='00:10:00',
+                # Slurm scheduler on Cori can be slow at times,
+                # increase the command timeouts
+                cmd_timeout=120,
+            ),
+        )
+    ]
+)
