4311c45dc6a6a0790b39db761f74f79b985c09a1
Seung Jin Sul <SSul@SSul-m69.local>
2020-11-30 16:37:42 -0800

    Minor change

--- jtm/jaws_jtm/jtm_manager.py
+++ jtm/jaws_jtm/jtm_manager.py
@@ -702,7 +702,7 @@ def recv_hb_from_worker_proc(hb_queue_name, log_dest_dir, b_resource_log):
     hb_msg = CONFIG.constants.HB_MSG
     jtm_worker_hb_exch = CONFIG.configparser.get("JTM", "jtm_worker_hb_exch")
     interval = CONFIG.configparser.getfloat("JTM", "client_hb_recv_interval")
-    hb_mgs_to_recv = CONFIG.configparser.getfloat("JTM", "heartbreat_message_count")
+    hb_mgs_to_recv = CONFIG.configparser.getfloat("JTM", "heartbeat_message_count")
 
     with RmqConnectionAmqpstorm(config=CONFIG).open() as conn:
         with conn.channel() as ch:
