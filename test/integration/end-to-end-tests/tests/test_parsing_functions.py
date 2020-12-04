e6b70578dd94c425922bcfbbc522c0c8593b20ef
Angie Kollmer <akollmer@lbl.gov>
2020-12-04 11:37:14 -0700

    remove test file becuase I deleted the function it was testing

--- test/integration/end-to-end-tests/tests/test_parsing_functions.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from JawsTestWrappers.parsing_functions import source_environment
-
-def test_source_environment(mocker):
-    mocker.patch('JawsTestWrappers.parsing_functions.submit_cmd',
-                 return_value='jaws-dev activated; see "jaws --help" for more.')
-    assert source_environment('dev') == 'source ~/jaws-dev.sh'
