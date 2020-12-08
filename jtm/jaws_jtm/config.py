59173d136fa0bd37cb5ecb000d27e28bb2a90a87
Stephan Trong <strong@lbl.gov>
2020-12-08 14:13:19 -0800

    rebase dev

--- jtm/jaws_jtm/config.py
+++ jtm/jaws_jtm/config.py
@@ -12,7 +12,7 @@ DEFAULT_CONFIG_FILE = 'jtm.ini'
 # CONSTANTS
 # -------------------------------------------------------------------------------------------
 class JtmConstants():
-    VERSION = "7.1.4"
+    VERSION = "7.1.5"
 
     # Supported cluster
     COMPUTE_RESOURCES = ["CORI",  # NERSC @ LBNL
