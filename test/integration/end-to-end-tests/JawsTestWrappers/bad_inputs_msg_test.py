c898c95f4186b65993273aa4e714f3296a23cbb7
Angie Kollmer <akollmer@lbl.gov>
2021-01-21 18:22:03 -0700

    bad inputs test in progress

--- test/integration/end-to-end-tests/JawsTestWrappers/bad_inputs_msg_test.py
+++ test/integration/end-to-end-tests/JawsTestWrappers/bad_inputs_msg_test.py
@@ -18,7 +18,7 @@ logging.basicConfig(filename='bad_inputs_msg.log',
 ANALYSIS_FILE_NAME = 'analysis.yaml'
 
 # this must match the name of a threshold file that has been submitted to autoqc before running this test
-THRESHOLD_FILE_NAME = 'jaws_run_success'
+THRESHOLD_FILE_NAME = 'bad_inputs_msg'
 
 check_tries = 50  # try this many times when waiting for a JAWS run to complete.
 check_sleep = 30  # wait for this amount of time between tries.
@@ -31,14 +31,20 @@ check_sleep = 30  # wait for this amount of time between tries.
 #
 
 def json_file_does_not_exist(e):
-    # check wh
-    if "File(s) not accessible:" in e:
+    # check for the correct error message
+    if "No such file or directory:" in e:
         final_dict['json_missing'] = 1
     else:
         final_dict['json_missing'] = 0
 
-def test_json_bad_path_to_input_file_msg():
-    pass
+
+def test_json_bad_path_to_input_file_msg(e):
+    # check for the correct error message
+    if "File(s) not accessible:" in e:
+        final_dict['bad_path_in_json'] = 1
+    else:
+        final_dict['bad_path_in_json'] = 0
+
 
 
 ####################
@@ -63,40 +69,41 @@ final_dict = {}
 # Submit WDLs & run Tests #
 ##########################
 #
-# Submit path to json file that does not exist
-# Can't use pf.submit_one_run_to_env here because it exits if submission not successful
+
 source_cmd = "source ~/jaws-%s.sh > /dev/null && " % args.environment
 wdl = "../TestsWDLs/jaws-alignment-example/main.wdl"
-json = "./FileDoesNotExist.json"
 out_dir = "out"  # nothing will get written here because these submissions are not accepted
 
+# Submit job with path to json file that does not exist
+# Can't use pf.submit_one_run_to_env here because it exits if submission not successful
+json = "./FileDoesNotExist.json"
 submit_cmd = "jaws run submit %s %s %s %s" % (wdl, json, out_dir, args.site)
-
 cmd = source_cmd + submit_cmd
 (o, e, r) = pf.submit_cmd(cmd)
 logging.info("cmd: %s\nout: %s\nerror: %s", cmd, o, e)
 
+# test for correct error message
 json_file_does_not_exist(e)
 
-
-# Submit json that points to non-existent input file
+# Submit job with json contains path to a non-existent input file
 # Can't use pf.submit_one_run_to_env here because it exits if submission not successful
-# json = "../TestsWDLs/bad-inputs/bad_path.json"
-# submit_cmd = "jaws run submit %s %s %s %s" % (wdl, json, out_dir, args.site)
+json = "../TestsWDLs/bad-inputs/bad_path.json"
+submit_cmd = "jaws run submit %s %s %s %s" % (wdl, json, out_dir, args.site)
 
-
+# test for correct error message
+test_json_bad_path_to_input_file_msg(e)
 #################################
 # create the analysis yaml file #
 #################################
 #
 # Put the analysis file in the run's output folder so that running multiple tests
 # at one time does not result in the analysis files overwriting one another
-#analysis_file_path = run_info['output_dir'] + '/' + ANALYSIS_FILE_NAME
+analysis_file_path = run_info['output_dir'] + '/' + ANALYSIS_FILE_NAME
 
 # create the name that will show up in the AutoQC report header by appending
 # the test name 'run_success' to the wdl file name
-#test_report_name = os.path.basename(args.wdl) + '_' + str(run_id)
-#logging.info(f"test_report_name: {test_report_name}\n")
+test_report_name = 'bad_inputs_test'
+logging.info(f"test_report_name: {test_report_name}\n")
 
 #pf.create_analysis_file(final_dict, analysis_file_path, test_report_name)
 #logging.info("create_analysis_file %s\n",analysis_file_path)
