ede5b988efa48d5f107ca55b578b6f41f8669384
Angie Kollmer <akollmer@lbl.gov>
2021-02-04 13:47:39 -0700

    Add run success test

--- test/integration/end-to-end-tests/score-card-tests/conftest.py
+++ test/integration/end-to-end-tests/score-card-tests/conftest.py
@@ -24,23 +24,24 @@ def submit_wdl_and_wait():
     This is a fixture that will submit a wdl for all functions to use.  
     This function returns the output of a wdl submission. 
     """
-#    cmd = ". ~jfroula/jaws-%s.sh > /dev/null 2>&1 && jaws run submit %s %s fq_count_out cori" % (env,wdl,inputs)
-#    (rc,stdout,stderr) = run(cmd)
-#    if rc > 0:
-#        pytest.exit("stderr: %s" % stderr)
-#   
-#    assert rc == 0
-#    data = json.loads(stdout)
+    cmd = ". ~/jaws-%s.sh > /dev/null 2>&1 && jaws run submit %s %s fq_count_out cori" % (env,wdl,inputs)
+    (rc,stdout,stderr) = run(cmd)
+    if rc > 0:
+        pytest.exit("stderr: %s" % stderr)
+
+    assert rc == 0
+    data = json.loads(stdout)
+
     # uncomment for testing
-    data={
-        "output_dir": "/global/cscratch1/sd/jfroula/JAWS/jaws/test/integration/end-to-end-tests/score-card-tests/fq_count_out",
-        "output_endpoint": "9d6d994a-6d04-11e5-ba46-22000b92c6ec",
-        "run_id": 16413,
-        "site_id": "CORI",
-        "status": "uploading",
-        "submission_id": "47a555c7-07a6-442c-a2f1-d0319f2e3008",
-        "upload_task_id": "444ac0b8-60f0-11eb-9905-0aa9ddbe2755"
-    }
+#    data={
+#        "output_dir": "/global/cscratch1/sd/jfroula/JAWS/jaws/test/integration/end-to-end-tests/score-card-tests/fq_count_out",
+#        "output_endpoint": "9d6d994a-6d04-11e5-ba46-22000b92c6ec",
+#        "run_id": 16413,
+#        "site_id": "CORI",
+#        "status": "uploading",
+#        "submission_id": "47a555c7-07a6-442c-a2f1-d0319f2e3008",
+#        "upload_task_id": "444ac0b8-60f0-11eb-9905-0aa9ddbe2755"
+#    }
     run_id = str(data['run_id'])
 
     # Wait for all the runs in run_ids list to finish.
