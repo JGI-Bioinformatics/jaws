9b58561b066460160eddd44a3bad020aaad88461
Angie Kollmer <akollmer@lbl.gov>
2021-02-11 18:49:16 -0700

    Add submission_utils file

---
+++ test/integration/end-to-end-tests/score-card-tests/submission_utils.py
@@ -0,0 +1,11 @@
+from subprocess import Popen, PIPE
+
+def run(cmd):
+    output = Popen(cmd, stdout=PIPE,
+             stderr=PIPE, shell=True,
+             universal_newlines=True)
+
+    stdout,stderr=output.communicate()
+    rc=output.returncode
+
+    return rc,stdout,stderr
