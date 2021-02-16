6a9f7722817aac89987920f5aa8084c7322a1de2
Angie Kollmer <akollmer@lbl.gov>
2021-02-15 18:05:43 -0700

    Move the submit functions back into conftest.py

--- test/integration/end-to-end-tests/score-card-tests/conftest.py
+++ test/integration/end-to-end-tests/score-card-tests/conftest.py
@@ -3,8 +3,52 @@ import json
 import pytest
 import smtplib
 import time
-import submission_utils as util
-import parsing_functions as pf
+
+
+def run(cmd):
+    output = Popen(cmd, stdout=PIPE,
+             stderr=PIPE, shell=True,
+             universal_newlines=True)
+
+    stdout,stderr=output.communicate()
+    rc=output.returncode
+
+    return rc,stdout,stderr
+
+
+def submit_wdl(env, wdl, input_json, outdir, site):
+    """
+    This is a fixture that will submit a wdl for all functions to use.
+    This function returns the output of a wdl submission.
+    """
+    if os.path.exists(outdir):
+        cmd = "rm -rf %s" % outdir
+        print(cmd)
+        (rc, stdout, stderr) = run(cmd)
+        if rc > 0:
+            os.exit("Failed to remove old output directory %s" % outdir)
+
+    cmd = "source ~/jaws-%s.sh && jaws run submit %s %s %s %s" % (env, wdl, input_json, outdir, site)
+    print(cmd)
+    (rc, stdout, stderr) = run(cmd)
+    if rc > 0:
+        pytest.exit("stderr: %s    stdout: " % stderr, stdout)
+
+    assert rc == 0
+    data = json.loads(stdout)
+
+    # uncomment for testing
+    # data={
+    #    "output_dir": "fq_count_out",
+    #    "output_endpoint": "9d6d994a-6d04-11e5-ba46-22000b92c6ec",
+    #    "run_id": 16405,
+    #    "site_id": "CORI",
+    #    "status": "uploading",
+    #    "submission_id": "47a555c7-07a6-442c-a2f1-d0319f2e3008",
+    #    "upload_task_id": "444ac0b8-60f0-11eb-9905-0aa9ddbe2755"
+    # }
+    return data
+
 
 
 # @pytest.fixture(scope="module")
@@ -75,7 +119,8 @@ def submit_fq_count_wdl(request):
     #site = getattr(request.module, "site")
     site = request.config.getoption("--site")
     #util.submit_wdl(env, wdl, input_json, outdir, site)
-    pf.submit_one_run_to_env(wdl, input_json, outdir, site, env)
+    submit_wdl(env, wdl, input_json, outdir, site)
+
 
 #
 # The next two functions allows us to use the --env to capture the environment [prod|staging|dev]. 
