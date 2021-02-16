ae6c01c06d8253c2c60632516eccfaa40ef68735
Angie Kollmer <akollmer@lbl.gov>
2021-02-15 18:48:09 -0700

    Add the pipe to dev/null back into the submit command in submission_utils

--- test/integration/end-to-end-tests/score-card-tests/submission_utils.py
+++ test/integration/end-to-end-tests/score-card-tests/submission_utils.py
@@ -27,7 +27,9 @@ def submit_wdl(env, wdl, input_json, outdir, site):
         if rc > 0:
             os.exit("Failed to remove old output directory %s" % outdir)
 
-    cmd = "source ~/jaws-%s.sh && jaws run submit %s %s %s %s" % (env, wdl, input_json, outdir, site)
+    # the pipe > /dev/null 2>&1 is needed below because otherwise the info printed from the
+    # activation command causes an error when we try to do json load later
+    cmd = "source ~/jaws-%s.sh > /dev/null 2>&1  && jaws run submit %s %s %s %s" % (env, wdl, input_json, outdir, site)
     print(cmd)
     (rc, stdout, stderr) = run(cmd)
     if rc > 0:
