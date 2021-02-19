f5c9a52fb4aeeb34a4d9a1a7db32bebe92994f82
Stephan Trong <strong@lbl.gov>
2021-02-19 10:59:53 -0800

    add files for installation

---
+++ monitor/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import os
+
+setup(
+    name="jaws_monitor",
+    version=os.popen("git describe --dirty=-dev --always --tags --abbrev=6")
+    .read()
+    .strip()
+    .replace("-", "+", 1),
+    description="JGI Analysis Workflow Service Prometheus Monitor Server",
+    url="https://code.jgi.doe.gov/advanced-analysis/jaws",
+    author="The JAWS Team",
+    packages=["jaws_prometheus"],
+    include_package_data=True,
+    install_requires=[line.strip() for line in open("requirements.txt")],
+    entry_points={
+        "console_scripts": ["jaws-prometheus = jaws_prometheus.jaws_prometheus:main", ]
+    },
+    zip_safe=False,
+)
