da7a0b3b1e109110d98d25cf7bd9b33ef07596a9
Stephan Trong <strong@lbl.gov>
2021-02-22 14:23:28 -0800

    clean up code duplication

--- monitor/jaws_prometheus/jaws_prometheus.py
+++ monitor/jaws_prometheus/jaws_prometheus.py
@@ -3,14 +3,14 @@
 This program serves as a prometheus custom exporter (otherwise known as a custom collector) for
 monitoring jaws components.
 
-The program runs as a daemon and serves as a light-weight web server. The prometheus server
-makes a HTTP request to this application to gather the reported metrics and stores the metrics into
+The program runs as a daemon and serves as a light-weight http server. The prometheus server
+makes an HTTP request to this application to gather the reported metrics and stores the metrics into
 the prometheus PromQL database for querying and web display using Grafana.
 
 A config file is used to both define the services to monitor and the mechanism to query the
 service for statuses. The supported mechanisms perform either an HTTP or RabbitMQ request to
-the service in order to determine it's health. The statuses are reported as metrics in
-a format for the prometheus server to consume.
+the service in order to determine it's health or aquire metrics. The statuses are reported
+in a specific format designed for the prometheus server to consume.
 
 The config file is either pass in as command line input or set as an environment variable
 using JAWS_MONITOR_CONFIG environment variable name.
@@ -20,13 +20,14 @@ The structure of the config file is in ini format and looks for the following se
 [MONITOR]    # required section
 port = port for this application
 
-[REST:BOOL]  # (optional) returns 0 or 1 designating whether the http request failed
+[HTTP:STATUS]  # (optional) returns 0 or 1 designating whether the http request failed
              # or is successful.
 name_of_service_1 = http://somehost:someport
 name_of_service_2 = http://somehost:someport
 ...
 
-[REST:FLOAT] # (optional) returns float from http request for returning disk free percent.
+[HTTP:DISK_FREE] # (optional) returns float from http request for returning disk free percent
+as float.
 name_of_service_3 = http://somehost:someport
 name_of_service_4 = http://somehost:someport
 ...
@@ -51,102 +52,34 @@ import os
 import requests
 import argparse
 import time
+import json
 import configparser
 from prometheus_client import start_http_server, Gauge
 
-# ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
-# sys.path.insert(0, os.path.join(ROOT_DIR, '../../rpc'))
+import sys
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(ROOT_DIR, '../../rpc'))
+
 from jaws_rpc import rpc_client
 
 
-class Config():
-    """Class to read and get config file entries.
+def configuration(config_file):
+    """Read the given config file and create a config parser object containing the
+    contents of the config file.
+
+    :param config_file: file path and name of config file
+    :type config_file: str
+    :return: none
     """
-    def __init__(self, config_file):
-        """Read the given config file and create a config parser object containing the
-        contents of the config file.
+    if not config_file:
+        config_file = os.environ.get('JAWS_MONITOR_CONFIG')
+    config = configparser.ConfigParser()
+    try:
+        config.read(config_file)
+    except Exception:
+        raise
 
-        :param config_file: file path and name of config file
-        :type config_file: str
-        :return: none
-        """
-        if not config_file:
-            config_file = os.environ.get('JAWS_MONITOR_CONFIG')
-        config = configparser.ConfigParser()
-        try:
-            config.read(config_file)
-        except Exception:
-            raise
-        self.config = config
-
-    def get_services(self):
-        """Parse the config entries and create a dictionary containing the following keys
-        for each component to be monitored:
-
-        function=name of function to call to get status
-        params=parameter(s) used to pass into function for getting status
-
-        The functions here are either for http requests or for rabbitmq requests.
-
-        Example of returned dict:
-        services = {
-            "central_prod": {
-                "function": http_respones
-                "params": "http://jaws.lbl.gov:5003/api/v2/status"
-            }
-            "site_cori_prod": {
-                "function":rmq_request
-                "params": rpc_client.RpcClient()
-            }
-        }
-
-        :param none
-        :type none
-        :return: none
-        """
-
-        services = {}
-
-        # Add monitoring for services that uses RMQ/RPC
-        for section in self.config.sections():
-            # Add monitoring for services that uses REST
-            if section == 'REST:BOOL':
-                for name in self.config[section]:
-                    services[name] = {
-                        'function': http_response,
-                        'params': self.config['REST:BOOL'].get(name)
-                    }
-
-            # Add monitoring for disk quota using REST
-            elif section == 'REST:FLOAT':
-                for name in self.config[section]:
-                    services[name] = {
-                        'function': disk_free,
-                        'params': self.config['REST:FLOAT'].get(name)
-                    }
-
-            # Add monitoring for services that uses RMQ/RPC
-            elif section.startswith('RMQ:'):
-                services[section.lower()] = {
-                    'function': rmq_request,
-                    'params': rpc_client.RpcClient(self.config[section]),
-                }
-
-        return services
-
-    def get_port(self):
-        """Parse the config entries to get the port number for serving up this web server using
-        the prometheus client module.
-
-        :param none
-        :type none
-        :return: none
-        """
-        port = self.config['MONITOR'].get('port')
-        if port:
-            return int(port)
-        else:
-            return None
+    return config
 
 
 def get_args():
@@ -162,6 +95,164 @@ def get_args():
     parser = argparse.ArgumentParser(description=prog_desc, formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('config_file', help='jaws monitor config file')
     return parser.parse_args()
+
+
+def get_services(config):
+    """Parse the config entries and create a dictionary containing the following keys
+    for each component to be monitored:
+
+    function=name of function to call to get status
+    args=parameter(s) used to pass into function for getting status
+
+    The functions here are either for http requests or for rabbitmq requests.
+
+    Example of returned dict:
+    services = {
+        "central_prod": {
+            "function": http_respones
+            "args": "http://jaws.lbl.gov:5003/api/v2/status"
+        }
+        "site_cori_prod": {
+            "function":rmq_request
+            "args": rpc_client.RpcClient()
+        }
+    }
+
+    :param none
+    :type none
+    :return: none
+    """
+
+    services = {}
+
+    # Add monitoring for services that uses RMQ/RPC
+    for section in config.sections():
+        # Add monitoring for services that uses REST
+        if section == 'HTTP:STATUS':
+            for name in config[section]:
+                services[name] = {
+                    'function': http_status,
+                    'args': config['HTTP:STATUS'].get(name)
+                }
+
+        # Add monitoring for disk quota using REST
+        elif section == 'HTTP:DISK_FREE':
+            for name in config[section]:
+                services[name] = {
+                    'function': disk_free,
+                    'args': config['HTTP:DISK_FREE'].get(name),
+                }
+
+        # Add monitoring for services that uses RMQ/RPC
+        elif section.startswith('RMQ:'):
+            services[section.lower()] = {
+                'function': rmq_request,
+                'args': rpc_client.RpcClient(config[section]),
+            }
+
+    return services
+
+
+def get_port(config):
+    """Parse the config entries to get the port number for serving up this web server using
+    the prometheus client module.
+
+    :param none
+    :type none
+    :return: none
+    """
+    port = config['MONITOR'].get('port')
+    if port:
+        return int(port)
+    else:
+        return None
+
+
+def is_valid_http_status(status_code):
+    """Check the HTTP status code and return 1 if response is successful or 0 if response
+    is bad.
+
+    :param status_code: HTTP status code
+    :type status_code: int
+    :return 0 or 1
+    :rtype: int
+    """
+    return 1 if status_code >= 200 and status_code < 300 else 0
+
+
+def convert_text_to_json(text):
+    """Convert json string to dictionary.
+
+    :param text: string
+    :return jsondata: json datastructure from input string
+    :rtype jsondata: dictionary or list
+    """
+    try:
+        jsondata = json.loads(text)
+    except ValueError:
+        jsondata = {}
+    return jsondata
+
+
+def http_status(url):
+    """Performs an HTTP request for the given url, checks the http status code for valid ranges and returns 0 or 1,
+    with 0 for bad response and 1 for successful response. Also returns the output of the http request.
+
+    :param url: url of HTTP request
+    :type url: string
+    :return status: 0 or 1
+    :rtype int
+    :return text: output of HTTP request
+    :rtype text: json
+    """
+    status_code, _ = http_request(url)
+    status = is_valid_http_status(status_code)
+    return status
+
+
+def disk_free(url):
+    """Performs an HTTP request for the given url. If status is successful, parse json
+    output from disk monitor and return disk free percentage as a float. Assumes
+    url returns json structure in the format:
+    {
+        'disk_free_pct': numeric
+    }
+    If HTTP response is unsuccessful, return -1.
+
+    :param url: url of HTTP request
+    :type url: string
+    :return disk_free_pct: percent of free disk from disk monitor
+    :rtype: float
+    """
+
+    disk_free_pct = -1
+    status_code, text = http_request(url)
+
+    status = is_valid_http_status(status_code)
+    if status:
+        jsondata = convert_text_to_json(text)
+        disk_free_pct = float(jsondata.get('disk_free_pct', 0))
+
+    return disk_free_pct
+
+
+def http_request(url):
+    """Performs an HTTP request and lookup the HTTP return status. If status 200-299, return 1
+    else return 0. If kwargs[tofloat] = True, return float form of value from http request.
+
+    :param url: url of HTTP request
+    :type url: string
+    :return status: http status code
+    :rtype status: int
+    :return text: output from http request
+    :rtype text: string
+    """
+    try:
+        r = requests.get(url=url)
+    except Exception:
+        return 500, {}
+
+    return r.status_code, r.text
 
 
 def rmq_request(rpc):
@@ -201,59 +292,6 @@ def create_proms(services):
     return proms
 
 
-def disk_free(*args, **rkwargs):
-    """Perform an HTTP request looking for a float like string as a return value. If the return
-    is successful, convert the string format to a float and return the float value.
-
-    :param *args: positional arguments; 1st element must be a url to the server program that
-        reports the disk free percentage
-    :type *args: list
-    :return retval: disk free percentage or -1 if http request fails.
-    :rtype retval: float | int
-    """
-    if not len(args) == 1:
-        raise SystemExit("Missing url parameter when calling rest_request()")
-    url = args[0]
-
-    retval = 0
-
-    try:
-        r = requests.get(url=url, **rkwargs)
-    except Exception:
-        return -1
-
-    if r.status_code >= 200 and r.status_code < 300:
-        try:
-            retval = float(r.text)
-        except ValueError:
-            retval = -1
-    else:
-        retval = -1
-
-    return retval
-
-
-def http_response(*args, **rkwargs):
-    """Performs an HTTP request and lookup the HTTP return status. If status 200-299, return 1
-    else return 0
-
-    :param *args: positional arguments; 1st element must be a url to the server program that
-        reports the disk free percentage
-    :type *args: list
-    :return retval: 0 or 1
-    :rtype retval: int
-    """
-    if not len(args) == 1:
-        raise SystemExit("Missing url parameter when calling http_response()")
-    url = args[0]
-
-    try:
-        r = requests.get(url=url, **rkwargs)
-    except Exception:
-        return 0
-    return 1 if r.status_code >= 200 and r.status_code < 300 else 0
-
-
 def process_request(services, proms, t=60):
     """Lookup the status for each service defined in the services dict (derived from the config
     file entries) and report the metrics using the prometheus client module.
@@ -272,10 +310,10 @@ def process_request(services, proms, t=60):
         if not func:
             raise SystemExit("Missing func key in services dict")
 
-        params = services[name].get('params')
-        status = func(params)
-        proms[name].set(status)
-        print(name, params, status)
+        args = services[name].get('args')
+        value = func(args)
+        print(name, args, value, type(value))
+        proms[name].set(value)
 
     time.sleep(t)
 
@@ -294,10 +332,10 @@ def main():
     config_file = args.config_file
 
     # Parse config file and define services to monitor
-    config = Config(config_file)
-    services = config.get_services()
+    config = configuration(config_file)
+    services = get_services(config)
 
-    port = config.get_port()
+    port = get_port(config)
     if not port:
         raise SystemExit("Failed to get port number for jaws_prometheus web server")
 
