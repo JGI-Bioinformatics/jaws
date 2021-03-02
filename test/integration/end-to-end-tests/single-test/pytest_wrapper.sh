82f925438c688a5ec1d7f5303fb68ea11d368b3a
Edward Steven Kirton <eskirton@lbl.gov>
2021-03-01 17:37:36 -0800

    update cicd vars for integration test

--- test/integration/end-to-end-tests/single-test/pytest_wrapper.sh
+++ test/integration/end-to-end-tests/single-test/pytest_wrapper.sh
@@ -1,33 +1,71 @@
 #!/bin/bash
 
-ENV=$1
-SITE=$2
-TEST_FOLDER=$3
+TEST_FOLDER=$1
 
 # check we have all arguments and environmental variables set
-if [[ ! $ENV ]] || [[ ! $SITE ]] || [[ ! $TEST_FOLDER ]]; then
-	echo "Usage: $0 <[prod|staging|dev]> <[cori|jgi]> <folder-with-tests>"
-	exit 1
-fi
-
-if [[ ! -d $SITE_JAWS_SW_BASEDIR ]]; then
-	echo "SITE_JAWS_SW_BASEDIR environmental variable not set"
-	exit 1
-fi
-if [[ ! $DEPLOYMENT_NAME ]]; then
-	echo "DEPLOYMENT_NAME environmental variable not set"
-	exit 1
-fi
-if [[ ! $JAWS_TEST_USER_TOKEN ]]; then
-	echo "JAWS_TEST_USER_TOKEN environmental variable not set"
+if [[ ! $TEST_FOLDER ]]; then
+	echo "Usage: $0 <folder-with-tests>"
 	exit 1
 fi
 
 
 set -euox pipefail
 
+## VALIDATE INPUT VARS
+## print list of all missing vars (instead of exiting on first undefined var)
+## exit with 1 if any vars are missing; 0 otherwise
+REQUIRED_VARS="
+JAWS_SITE
+DEPLOYMENT_NAME
+JAWS_TEST_TOKEN
+"
+RESULT=0
+echo "VALIDATING REQUIRED VARS"
+for VAR in $REQUIRED_VARS; do
+  if [[ -z ${!VAR} ]]; then
+    echo "Missing env var: $VAR">&2
+    RESULT=1
+  fi
+  echo "... $VAR : OK"
+done
+[ $RESULT -eq 0 ] || exit $RESULT
+
+
+## SITE-SPECIFIC VARIABLES:
+## This will define global vars with a SITE_ prefix, with values from variables referenced by the JAWS_SITE
+## variable.  For instance, if JAWS_SITE="JGI", setting GLOBUS_EP site var will define a SITE_GLOBUS_EP var
+## with the value from JGI_GLOBUS_EP.
+## This allows the site installation scripts to use the SITE_* vars.
+echo "DEFINING SITE VARS"
+function set_site_var {
+  VAR_NAME="$1"
+  SRC_VAR_NAME="${JAWS_SITE}_${VAR_NAME}"
+  if [ -z ${!SRC_VAR_NAME+xxx} ]; then
+    echo "Missing env var: $SRC_VAR_NAME">&2
+    exit 1
+  fi
+  SITE_VAR_NAME="SITE_${VAR_NAME}"
+  VALUE="${!SRC_VAR_NAME}"
+  echo "... $SITE_VAR_NAME"
+  export $SITE_VAR_NAME
+  printf -v "$SITE_VAR_NAME" "%s" "$VALUE"
+}
+SITE_VARS="
+INSTALL_BASEDIR
+JAWS_SW_BASEDIR
+"
+for SITE_VAR in $SITE_VARS; do
+  set_site_var $SITE_VAR
+done
+
+
+## DEFINE VARS FROM OTHER VARS
+echo "DEFINING PATHS"
+export INSTALL_DIR="$SITE_INSTALL_BASEDIR/jaws-$DEPLOYMENT_NAME"
+export SITE_CLIENT_INSTALL_DIR="$SITE_JAWS_SW_BASEDIR/jaws-$DEPLOYMENT_NAME"
+
 # source venv
-source $SITE_JAWS_SW_BASEDIR/jaws-$DEPLOYMENT_NAME/bin/activate
+source "$SITE_CLIENT_INSTALL_DIR/bin/activate"
 
 # write token to homedir
 echo << END > ~/jaws.conf
@@ -39,4 +77,4 @@ END
 chmod 600 ~/jaws.conf
 
 cd test/integration/end-to-end-tests
-pytest --verbose --env $ENV --site $SITE $TEST_FOLDER
+pytest --verbose --env $DEPLOYMENT_NAME --site $JAWS_SITE $TEST_FOLDER
