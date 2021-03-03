2db9a5d08727579162fcb48f95f5c136d901e3c3
Jeff Froula <jlfroula@lbl.gov>
2021-03-03 12:36:09 -0800

    removed env and moved pytest_wrapper.py to parent

--- test/integration/end-to-end-tests/conftest.py
+++ test/integration/end-to-end-tests/conftest.py
@@ -10,121 +10,118 @@ check_sleep=30
 
 @pytest.fixture(scope="session",autouse=True)
 def test_for_all_args(request):
-    env = request.config.getoption("--env")
+    target_dir = request.config.getoption("--dir")
     site = request.config.getoption("--site")
-    if not env or not site: 
-        pytest.exit("Error: You are missing some arguments?\nUsage: pytest -n <number of tests in parallel> --capture=<[yes|no]> --verbose --env <[prod|staging|dev]> --site <[cori|jgi]> <directory or file>")
+    if not target_dir or not site: 
+        pytest.exit("Error: You are missing some arguments?\nUsage: pytest -n <number of tests in parallel> --capture=<[yes|no]> --verbose --dir <directory with tests> --site <[cori|jgi]> <directory or file>")
 
 @pytest.fixture(scope="session")
 def submit_fq_count_wdl(request):
-    wdl = "./WDLs/fq_count.wdl"
-    input_json = "./test-inputs/fq_count.json"
-
     # allow user to pass variables into the test functions via command line
-    env = request.config.getoption("--env")
+    target_dir = request.config.getoption("--dir")
     site = request.config.getoption("--site")
+
+    wdl = target_dir + "/WDLs/fq_count.wdl"
+    input_json = target_dir + "/test-inputs/fq_count.json"
 
     data = util.submit_wdl(wdl, input_json, site)
     return data
 
 @pytest.fixture(scope="session")
 def submit_subworkflow_alignment(request):
-    wdl = "./WDLs/jaws-alignment-example/main.wdl"
-    input_json = "./WDLs/jaws-alignment-example/inputs.json"
-
     # allow user to pass variables into the test functions via command line
-    env = request.config.getoption("--env")
+    target_dir = request.config.getoption("--dir")
     site = request.config.getoption("--site")
 
-    data = util.submit_wdl(env, wdl, input_json, site)
+    wdl = target_dir + "/WDLs/jaws-alignment-example/main.wdl"
+    input_json = target_dir + "/WDLs/jaws-alignment-example/inputs.json"
+
+    data = util.submit_wdl(wdl, input_json, site)
 
     # wait for run to complete
     run_id = data['run_id']
-    util.wait_for_run(env,run_id,check_tries,check_sleep)
+    util.wait_for_run(run_id,check_tries,check_sleep)
     # print(data)  # used for debugging
     return data
 
 
 @pytest.fixture(scope="session")
 def submit_bad_task(request):
-    wdl = "./WDLs/bad_task.wdl"
-    input_json = "./test-inputs/fq_count.json"
-
     # allow user to pass variables into the test functions via command line
-    env = request.config.getoption("--env")
+    target_dir = request.config.getoption("--dir")
     site = request.config.getoption("--site")
 
-    data = util.submit_wdl_noexit(env, wdl, input_json, site)
+    wdl = target_dir + "/WDLs/bad_task.wdl"
+    input_json = target_dir + "/test-inputs/fq_count.json"
+
+    data = util.submit_wdl_noexit(wdl, input_json, site)
     run_id = data['run_id']
 
     # wait for run to complete
-    util.wait_for_run(env,run_id,check_tries,check_sleep)
+    util.wait_for_run(run_id,check_tries,check_sleep)
     return data
 
 
 @pytest.fixture(scope="session")
 def submit_bad_docker(request):
-    wdl = "./WDLs/bad_docker.wdl"
-    input_json = "./test-inputs/fq_count.json"
-
     # allow user to pass variables into the test functions via command line
-    env = request.config.getoption("--env")
+    target_dir = request.config.getoption("--dir")
     site = request.config.getoption("--site")
 
-    data = util.submit_wdl(env, wdl, input_json, site)
+    wdl = target_dir + "/WDLs/bad_docker.wdl"
+    input_json = target_dir + "/test-inputs/fq_count.json"
+
+    data = util.submit_wdl(wdl, input_json, site)
 
     # wait for run to complete
     run_id = data['run_id']
-    util.wait_for_run(env,run_id,check_tries,check_sleep)
+    util.wait_for_run(run_id,check_tries,check_sleep)
 
     # print(data)  # used for debugging
     return data
 
 @pytest.fixture(scope="session")
 def submit_skylake_250(request):
-    wdl = "./WDLs/skylake_test_250.wdl"
-    input_json = "./test-inputs/fq_count.json"
+    # allow user to pass variables into the test functions via command line
+    target_dir = request.config.getoption("--dir")
+
+    wdl = target_dir + "/WDLs/skylake_test_250.wdl"
+    input_json = target_dir + "/test-inputs/fq_count.json"
     site="cori"
 
-    # allow user to pass variables into the test functions via command line
-    env = request.config.getoption("--env")
-
-    data = util.submit_wdl(env, wdl, input_json, site)
+    data = util.submit_wdl(wdl, input_json, site)
 
     # wait for run to complete
     run_id = data['run_id']
-    util.wait_for_run(env,run_id,check_tries,check_sleep)
+    util.wait_for_run(run_id,check_tries,check_sleep)
 
     return data
 
 @pytest.fixture(scope="session")
 def submit_skylake_500(request):
-    wdl = "./WDLs/skylake_test_500.wdl"
-    input_json = "./test-inputs/fq_count.json"
-    site="cori"
-
     # allow user to pass variables into the test functions via command line
-    env = request.config.getoption("--env")
+    target_dir = request.config.getoption("--dir")
+    site = request.config.getoption("--site")
 
-    data = util.submit_wdl(env, wdl, input_json, site)
+    wdl = target_dir + "/WDLs/skylake_test_500.wdl"
+    input_json = target_dir + "/test-inputs/fq_count.json"
+
+    data = util.submit_wdl(wdl, input_json, site)
 
     # wait for run to complete
     run_id = data['run_id']
-    util.wait_for_run(env,run_id,check_tries,check_sleep)
+    util.wait_for_run(run_id,check_tries,check_sleep)
 
     return data
 
 
-#
-# The next two functions allows us to use the --env to capture the environment [prod|staging|dev]. 
-# This environment is an argument that can be passed into the test functions
-#
+# The addoption functions allows us to use flags to capture arguments on the command line.
 def pytest_addoption(parser):
     parser.addoption(
-        "--env",
+        "--dir",
         action="store",
         default=[],
-        help="testing environment [prod|staging|dev] passed to test functions",
+        help="this is the path to where the WDLs and input.json files are."
     )
     parser.addoption(
         "--site",
@@ -133,17 +130,12 @@ def pytest_addoption(parser):
         help="the JAWS site [cori|jgi] that will be used during submission",
     )
 
+# These functions allows an argument to be passed into the test functions
 @pytest.fixture
-def env(request):
-    return request.config.getoption("--env")
+def dir(request):
+    return request.config.getoption("--dir")
 
 @pytest.fixture
 def site(request):
     return request.config.getoption("--site")
 
-
-# def pytest_generate_tests(metafunc):
-#     if "env" in metafunc.fixturenames:
-#         metafunc.parametrize("env", metafunc.config.getoption("env"))
-#     if "site" in metafunc.fixturenames:
-#         metafunc.parametrize("site", metafunc.config.getoption("site"))
