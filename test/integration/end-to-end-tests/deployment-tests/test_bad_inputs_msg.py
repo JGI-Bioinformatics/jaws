73790c01948265bf619d1df53972fb46e7f6158f
Angie Kollmer <akollmer@lbl.gov>
2021-03-04 14:41:37 -0700

    updating test_run_success and test_bad_inputs

--- test/integration/end-to-end-tests/deployment-tests/test_bad_inputs_msg.py
+++ test/integration/end-to-end-tests/deployment-tests/test_bad_inputs_msg.py
@@ -1,48 +1,48 @@
 #!/usr/bin/env python
-"""This will submit a WDL to JAWS and wait for completion, 
-   run tests and finally create a yaml file for the autoqc GUI.
+"""These functions are to test the "testcases" from the "score_card" integration tests.
+google doc: https://docs.google.com/document/d/1nXuPDVZ3dXl0AetyU5Imdbi0Gvc5sUhAR0OfYxss2uI/edit#heading=h.rmy1jmsa0m7n
+google sheet: https://docs.google.com/spreadsheets/d/1eBWvk4FSPpbFclTuzu0o77aPAxcZ78C_mVKCnHoMMAo/edit#gid=1883830451
 
-   Author: Angie Koller <akoller@lbl.gov>
-   Updated: 01/23/21
+This library of tests uses "fixtures" from conftest.py which should be located in the same directory.
+There is no need to import conftest.py as it is done automatically.
+
+Author: Angie Kollmer <akollmer@lbl.gov>
+Updated: 03/04/21
 """
 
-import sys
-import os
-import argparse
 import json
-import logging
-import parsing_functions as pf
+import pytest
+import submission_utils as util
 
 check_tries = 50  # try this many times when waiting for a JAWS run to complete.
 check_sleep = 30  # wait for this amount of time between tries.
 
-def test_json_file_does_not_exist(env):
+def test_json_file_does_not_exist(env, site):
     # Submit job with path to json file that does not exist
     # Can't use pf.submit_one_run_to_env here because it exits if submission not successful
     json = "./FileDoesNotExist.json"
 
-    # TODO pass site info in through command line also
     source_cmd = "source ~/jaws-%s.sh > /dev/null && " % env
-    submit_cmd = "jaws run submit %s %s %s %s" % (wdl, json, out_dir, "cori")
+    submit_cmd = "jaws run submit %s %s %s %s" % (wdl, json, out_dir, site)
     cmd = source_cmd + submit_cmd
-    (r,o,e) = util.run(cmd)
+    (r, o, e) = util.run(cmd)
     print("cmd: %s\nout: %s\nerror: %s", cmd, o, e)
 
     # check for the correct error message
     assert "No such file or directory:" in e
 
 
-def test_json_bad_path_to_input_file_msg(env):
+def test_json_bad_path_to_input_file_msg(env, site):
     source_cmd = "source ~/jaws-%s.sh > /dev/null && " % env
     wdl        = "~/jaws/examples/jaws-alignment-example/main.wdl"
-    out_dir    = pf.timestamp_dir("./out/bad_inputs")
+    out_dir    = "./out/bad_inputs"
 
     # Submit job with json contains path to a non-existent input file
     # Can't use pf.submit_one_run_to_env here because it exits if submission not successful
     json = "../test-inputs/bad_path_inputs.json"
-    submit_cmd = "jaws run submit %s %s %s %s" % (wdl, json, out_dir, "cori")
+    submit_cmd = "jaws run submit %s %s %s %s" % (wdl, json, out_dir, site)
     cmd = source_cmd + submit_cmd
-    (r,o,e) = util.run(cmd)
+    (r, o, e) = util.run(cmd)
     # print("cmd: %s\nout: %s\nerror: %s", cmd, o, e)
 
     # check for the correct error message
