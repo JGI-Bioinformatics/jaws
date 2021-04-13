a45b9e6c5fc947a337ace25bd43234a0aec8e2d6
Edward Steven Kirton <eskirton@lbl.gov>
2021-04-12 17:51:08 -0700

    move RunLog class to runs.py

--- central/jaws_central/runs.py
+++ central/jaws_central/runs.py
@@ -480,6 +480,62 @@ class Run:
         log.add_log(status_from, self.model.status, self.model.updated, reason)
 
 
+class RunLog:
+
+    def __init__(self, session, run_id):
+        self.session = session
+        self.id = run_id
+
+    def run_log(self):
+        """
+        Retrieve complete log of a Run's state transitions.
+
+        :return: Table of log entries
+        :rtype: list
+        """
+        logger.info(f"User {self.user.id}: Get log of Run {self.id}")
+        try:
+            query = (
+                self.session.query(models.Run_Log)
+                .filter_by(run_id=run_id)
+                .order_by(models.Run_Log.timestamp)
+            )
+        except SQLAlchemyError as error:
+            logger.exception(f"Error selecting from run_logs: {error}")
+            abort(500, f"Db error; {error}")
+        table = []
+        for log in query:
+            reason = log.reason if log.reason else ""
+            row = [
+                log.status_from,
+                log.status_to,
+                log.timestamp.strftime("%Y-%m-%d %H:%M:%S"),
+                reason,
+            ]
+            table.append(row)
+        return table
+
+    def _insert_log(self, log_entry)
+        try:
+            self.session.add(log_entry)
+            self.session.commit()
+        except Exception as error:
+            self.session.rollback()
+            logger.error(
+                f"Error while adding run log entry to cancel run {run.id}: {error}"
+            )
+
+    def add_log(self, status_from, status_to, timestamp, reason=None): 
+        log_entry = models.Run_Log(
+            run_id=run.id,
+            status_from=status_from,
+            status_to=run.status,
+            timestamp=run.updated,
+            reason=reason,
+        )
+        self._insert_log(log_entry)
+
+
 def queue(session, user):
     """Return the current user's unfinished runs.
 
