f9e0824fe59625657100dcf3104eea9eece4b183
Edward Steven Kirton <eskirton@lbl.gov>
2021-04-27 13:13:34 -0700

    rpc_operations now uses Run object instead of insert into db directly

--- central/jaws_central/rpc_operations.py
+++ central/jaws_central/rpc_operations.py
@@ -1,11 +1,10 @@
 """
-RPC operations by which Sites update Run and Job status.
+RPC operations used by jaws-sites.
 """
 
 import logging
-import sqlalchemy.exc
 from datetime import datetime
-from jaws_central.models_sa import Run, Run_Log
+from jaws_central.runs import Run
 from jaws_rpc.responses import success, failure
 
 
@@ -14,7 +13,7 @@ logger = logging.getLogger(__package__)
 
 def update_run_logs(params, session):
     """
-    Receive a run status update from a Site, insert into run_logs table and update state in runs table.
+    Receive a run status update from a Site.
 
     :param params: one run log entry
     :type params: dict
@@ -23,57 +22,13 @@ def update_run_logs(params, session):
     """
     site_id = params["site_id"]
     run_id = int(params["run_id"])
-    status_from = params["status_from"]
     status_to = params["status_to"]
-    timestamp = datetime.strptime(params["timestamp"], "%Y-%m-%d %H:%M:%S")
-    reason = None
-    if "reason" in params:
-        reason = params["reason"]
+    params["timestamp"] = datetime.strptime(params["timestamp"], "%Y-%m-%d %H:%M:%S")
     logger.info(f"Run {run_id} at Site {site_id} now {status_to}")
-
-    # DEFINE ROW OBJ
     try:
-        log = Run_Log(
-            run_id=run_id,
-            status_from=status_from,
-            status_to=status_to,
-            timestamp=timestamp,
-            reason=reason,
-        )
+        run = Run(session, run_id)
+        run.update_status(**params)
     except Exception as error:
-        return failure(error)
-
-    # INSERT OR IGNORE INTO RUN-LOGS TABLE
-    try:
-        session.add(log)
-        session.commit()
-    except sqlalchemy.exc.IntegrityError:
-        session.rollback()
-        logger.warning(f"Run log {run_id}:{status_to} is duplicate; ignored")
-        return success()
-    except Exception as error:
-        session.rollback()
-        return failure(error)
-
-    # UPDATE RUNS TABLE
-    run = session.query(Run).get(run_id)
-    if run.status == status_to:
-        # ignore redundant state change (duplicate message)
-        return success()
-    run.status = status_to
-    run.updated = timestamp
-    if status_to == "submitted":
-        run.cromwell_run_id = params["cromwell_run_id"]
-    elif status_to == "downloading":
-        run.download_task_id = params["download_task_id"]
-    elif status_to == "succeeded":
-        run.result = "succeeded"
-    elif status_to == "failed":
-        run.result = "failed"
-    try:
-        session.commit()
-    except Exception as error:
-        session.rollback()
         return failure(error)
     return success()
 
@@ -88,6 +43,7 @@ operations = {
             "status_from",
             "status_to",
             "timestamp",
+            "reason",
         ],
     },
 }
