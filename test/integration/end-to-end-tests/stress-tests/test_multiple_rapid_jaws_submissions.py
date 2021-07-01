19a4a9d68d83605aff32dbd6e909c8e2b0d8aac4
Angie Kollmer <akollmer@lbl.gov>
2021-07-01 14:49:30 -0600

    troubleshooting

--- test/integration/end-to-end-tests/stress-tests/test_multiple_rapid_jaws_submissions.py
+++ test/integration/end-to-end-tests/stress-tests/test_multiple_rapid_jaws_submissions.py
@@ -9,20 +9,21 @@ import submission_utils as util
 # This means that increasing the number of wdl/json combinations will require updating -n value for
 # the gitlab-ci.yml entries for this test
 
+
 class TestMultipleRapidJawsSubmissions:
 
     @pytest.mark.parametrize(
         "wdl, input_json", "tag", "check_tries", "check_sleep",
-        (
+        [
             (
                 "../../../examples/jaws-alignment-example/alignment.wdl",
                 "../../../examples/jaws-alignment-example/inputs.align.json",
                 "alignment-stress-test",
                 360, 100,
-            ),
-        ),
+            )
+        ]
     )
-    def test_tutorial_success(self, env, site, wdl, input_json, tag, check_tries, check_sleep):
+    def test_rapid_submissions(self, env, site, wdl, input_json, tag, check_tries, check_sleep):
         # run the test against all the wdl/json in the @pytest.mark.parametrize
 
         jaws_output = util.submit_wdl(env, wdl, input_json, site, tag)
