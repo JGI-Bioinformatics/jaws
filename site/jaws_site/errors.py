39eb5efde51e27863a06f58f6bcbd65b103456de
Edward Kirton <eskirton@lbl.gov>
2021-10-19 12:57:28 -0700

    update errors report to use new nested cromwell errors data structure

--- site/jaws_site/errors.py
+++ site/jaws_site/errors.py
@@ -1,4 +1,5 @@
 import logging
+import copy
 from jaws_site import config
 from jaws_site.cromwell import Cromwell, CromwellError
 from jaws_site.tasks import get_task_log_error_messages
@@ -8,7 +9,9 @@ logger = logging.getLogger(__package__)
 
 
 def _get_cromwell_errors_report(cromwell_run_id):
-    """Get errors report from Cromwell metadata.
+    """
+    Get errors report from Cromwell metadata.
+
     :param cromwell_run_id: Cromwell's UUID for the run
     :type cromwell_run_id: str
     :return: errors for each failed task
@@ -19,8 +22,46 @@ def _get_cromwell_errors_report(cromwell_run_id):
     return metadata.errors()
 
 
+def _add_task_log_errors(session, report):
+    """
+    Recursive function to parse "calls" section of errors struct and add
+    error messages from task log.
+
+    :param session: db session handle
+    :type session: sqlalchmey.session
+    :param report: report dictionary with "calls"
+    :type report: dict
+    :return: new report dictionary with "calls" and task log errors added
+    :rtype: dict
+    """
+    calls = report["calls"]
+    new_calls = {}
+    for task_name, task_calls in calls.items():
+        # the errors report only contains the last attempt (i.e. exactly 1 call)
+        new_calls[task_name] = []
+        call = task_calls[0]
+        new_call = {}
+        if "subWorkflowMetadata" in call:
+            # this is a subworkflow, so recurse
+            new_call["subWorkflowMetadata"] = _add_task_log_errors(
+                session, call["subWorkflowMetadata"]
+            )
+        else:
+            # this is a regular task
+            new_call = copy.deepcopy(call)
+            if "jobId" in call:
+                # the task was submitted to the back-end so check task-log for errors
+                new_call["taskLog"] = get_task_log_error_messages(
+                    session, call["jobId"]
+                )
+        new_calls[task_name].append(new_call)
+    new_report = {"calls": new_calls}
+    return new_report
+
+
 def get_errors(session, cromwell_run_id):
-    """Retrieve error messages from Cromwell Metadata and TaskLog.
+    """
+    Retrieve error messages from Cromwell Metadata and TaskLog.
 
     :param cromwell_run_id: Cromwell run ID
     :type params: dict
@@ -28,15 +69,17 @@ def get_errors(session, cromwell_run_id):
     :rtype: dict
     """
     try:
-        full_errors_report = _get_cromwell_errors_report(cromwell_run_id)
+        errors_report = _get_cromwell_errors_report(cromwell_run_id)
     except CromwellError as error:
-        logger.error(f"Failed to retrieve Cromwell errors report for {cromwell_run_id}: {error}")
+        logger.error(
+            f"Failed to retrieve Cromwell errors report for {cromwell_run_id}: {error}"
+        )
         return {}
-    for a_cromwell_run_id, errors_report in full_errors_report.items():
-        if "calls" in errors_report:
-            for task_name in errors_report["calls"]:
-                for item in errors_report["calls"][task_name]:
-                    if "jobId" in item:
-                        cromwell_job_id = item["jobId"]
-                        item["taskLog"] = get_task_log_error_messages(session, cromwell_job_id)
-    return full_errors_report
+
+    # if there are no errors, there will not be a "calls" section
+    if "calls" not in errors_report:
+        return errors_report
+
+    # generate new errors report with any task-log error messages added
+    new_report = _add_task_log_errors(session, errors_report)
+    return new_report
