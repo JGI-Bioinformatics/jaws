dcff0792648eea0461d273727adc5d57b1489b87
Jeff Froula <jlfroula@lbl.gov>
2021-10-30 10:11:12 -0700

    removed spaces

--- test/integration/end-to-end-tests/conftest.py
+++ test/integration/end-to-end-tests/conftest.py
@@ -150,9 +150,9 @@ def submit_skylake_250(request):
     """ This will submit to the large memory skylake machine on cori. The fixture will be skipped if run on anything other than cori.
 
     :param a request object for capturing CLI arguments
-    :type object 
-    :rtype dictionary 
-    :return output from jaws submit 
+    :type object
+    :rtype dictionary
+    :return output from jaws submit
     """
 
     # allow user to pass variables into the test functions via command line
@@ -232,8 +232,8 @@ def pytest_addoption(parser):
     """ The parser.addoption function allows us to use flags to capture CLI arguments that can then be used in our test functions as if they were fixtures. 
 
     :param a request object for capturing CLI arguments
-    :type object 
-    :rtype none 
+    :type object
+    :rtype none
     :return none
     """
     parser.addoption(
