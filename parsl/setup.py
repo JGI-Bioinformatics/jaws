36f92cd8c26ec5a8cc1fcebd1b713f63241d1867
Mario Melara <maamelara@gmail.com>
2021-11-08 16:09:50 -0800

    Add parsl_configs subpackage to setup.py
    Closes #1081
    
    Adds missing parsl_configs subpackage to setup.py for install

--- parsl/setup.py
+++ parsl/setup.py
@@ -10,7 +10,8 @@ setup(
     description="JGI Analysis Workflow Service Parsl Backend",
     url="https://code.jgi.doe.gov/advanced-analysis/jaws",
     author="The JAWS Team",
-    packages=["jaws_parsl"],
+    packages=["jaws_parsl",
+              "jaws_parsl.parsl_configs"],
     include_package_data=True,
     install_requires=[line.strip() for line in open("requirements.txt")],
     entry_points={
