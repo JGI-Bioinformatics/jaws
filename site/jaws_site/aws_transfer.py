8ae9055879929cea76d543c6811130220c675a33
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2021-12-21 16:04:27 -0800

    Handles threading for transfers
    Each _submit_transfer creates a thread in a dict and checks the thread in transfer status by looking for the thread in the dict.

--- site/jaws_site/aws_transfer.py
+++ site/jaws_site/aws_transfer.py
@@ -3,6 +3,11 @@ import os
 from typing import Dict
 import jaws_site.config
 
+import threading
+
+# For creating random uuid
+import uuid
+
 # For aws transfers we'll use boto3
 import boto3
 from botocore.exceptions import ClientError
@@ -13,9 +18,8 @@ logger = logging.getLogger(__package__)
 class DataTransferFactory:
     def __init__(self) -> None:
         self._config = jaws_site.config.Configuration()
-        pass
 
-    def submit_transfer(self) -> Dict:
+    def submit_transfer(self, label, src_site_id, dest_site_id, manifest_file) -> Dict:
         """
         label : human readable label (e.g. "Upload Run 2552") -- optional
         src_site_id (e.g. "cori", "jgi", "tahoma")
@@ -27,7 +31,7 @@ class DataTransferFactory:
         """
         pass
 
-    def _add_transfer(self):
+    def _add_transfer(self) -> str:
         """Insert transfer into table with "queued" initial state"""
         pass
 
@@ -53,6 +57,7 @@ class DataTransferS3(DataTransferFactory):
     def __init__(self) -> None:
         # Init the DataTransfer  Factory to get self._config
         super().__init__(self)
+        logger.debug(f"Creating DataTransferS3")
 
         self.host_path = self._config.get("AWS", "host_path")
         self.s3_bucket = self._config.get("AWS", "s3_bucket")
@@ -67,14 +72,68 @@ class DataTransferS3(DataTransferFactory):
             aws_secret_access_key=self.aws_secret_access_key,
         )
 
-    def submit_transfer(self, label, dest_endpoint, src_dir, dest_dir) -> Dict:
-        self._add_transfer()
-        self._submit_transfer(transfer_id="")
+        self._transfer_threads = dict()
 
-    def _submit_transfer(self, transfer_id):
-        self._aws_client.upload_file(
-            "FILE_NAME",
-            self.s3_bucket,
-            "OBJECT_NAME",
-            ExtraArgs={"Metadata": {"mykey": "myvalue"}},
+    def _add_transfer(self) -> str:
+        """
+        Return an id associated with transfer
+
+        Create a random uuid for aws transfer
+            -> Could also send to a database
+        """
+        return str(uuid.uuid4())
+
+    def submit_transfer(self, label, src_dir, dest_dir, manifest_file) -> Dict:
+
+        transfer_id = self._add_transfer()
+        logger.debug(f"S3 Transfer starting for {transfer_id} {label}")
+
+        return dict(transfer_id=transfer_id)
+
+    def _submit_transfer(self, transfer_id, filename, src_dir, dest_dir, label):
+        # Object name inside of S3 bucket
+        # Can include a folder name inside bucket as dest_dir
+        object_name = f"{dest_dir}/{filename}"
+
+        filename = os.path.basename(src_dir)
+        extra_args = {"Metadata": {"label": label}}
+
+        # Create a thread to transfer data
+        #   Could run into issue with lots of threads transfering data
+        #   Look into making a queue to start transfers
+        self._transfer_threads[transfer_id] = threading.Thread(
+            target=self._aws_client.upload_file,
+            args=(src_dir, self.s3_bucket, object_name),
+            kwargs={"ExtraArgs": extra_args},
         )
+
+        # Start thread running and return
+        self._transfer_thread[transfer_id].start()
+        return True
+
+    def transfer_status(self, transfer_id):
+        """
+        Return the status of the transfer
+        """
+        try:
+            # While transfering thread "is_alive" -> True
+            # When done transfering thread "is_alive" -> False
+            alive = self._transfer_thread[transfer_id].is_alive()
+        except KeyError:
+            # If the uuid is out of the
+            logging.error(f"UUID: {transfer_id} not in table")
+            return "failed"
+        except Exception as e:
+            logging.error(e)
+            return "failed"
+
+        if not alive:
+            try:
+                self._transfer_thread[transfer_id].join()
+                self._transfer_thread.pop(transfer_id, None)
+                return "upload complete"
+            except ClientError as e:
+                logging.error(e)
+                return "failed"
+        else:
+            return "uploading"
