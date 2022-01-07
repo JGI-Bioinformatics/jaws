d25b4111cabe46d28e18e3a3067fa3fcabe6b448
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2022-01-07 13:51:31 -0800

    Add pagurus to docker script

--- test/integration/docker_exec.sh
+++ test/integration/docker_exec.sh
@@ -7,5 +7,19 @@ IMAGE=$5
 JOB_SHELL=$6
 SCRIPT=$7
 echo "Executing on $HOSTNAME" 1>&2
+
+# Start monitoring running, get monitor pid and sleep
+pagurus -u $USER -o $PWD/stats.csv &
+export PID=$!
+sleep 2
+
+# Run container script and catch exit code
 docker run --rm -it -v $REMOTE_REF:$LOCAL_REF -v $CWD:$DOCKER_CWD --workdir=$DOCKER_CWD --entrypoint=$JOB_SHELL $IMAGE $SCRIPT
-exit $?
+export EXIT_CODE=$?
+
+# Kill pagurus monitoring and wait for it to write stats.csv
+kill $PID
+sleep 5
+
+# Return with container exit code
+exit $EXIT_CODE
