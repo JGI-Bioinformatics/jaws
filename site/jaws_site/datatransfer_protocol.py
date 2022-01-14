7bd26a56786d3bfd4189351b9cd7f2ab45742dd9
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2022-01-14 14:30:07 -0800

    Move changes from !1077 to here

---
+++ site/jaws_site/datatransfer_protocol.py
@@ -0,0 +1,36 @@
+from typing import Callable, Protocol
+import importlib
+
+
+class DataTransferError(Exception):
+    pass
+
+
+class DataTransferProtocol(Protocol):
+    def submit_transfer(self):
+        ...
+
+    def transfer_status(self):
+        ...
+
+    def cancel(self):
+        ...
+
+
+class DataTransferFactory:
+    def __new__(cls, obj_type: str) -> Callable:
+        classname = "DataTransfer"
+        modulename = f"datatransfer_plugins.{obj_type}"
+
+        try:
+            imported_module = importlib.import_module(modulename)
+        except ImportError:
+            raise DataTransferError(f"No module {modulename} found")
+
+        try:
+            obj = getattr(imported_module, classname)
+        except AttributeError as err:
+            msg = f"{imported_module} cannot be found or has no class named {classname}: {err}"
+            raise DataTransferError(msg)
+
+        return obj()
