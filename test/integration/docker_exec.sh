2cfd2a5a9c16474e16a957cd00e5186d4dbc3cde
Nicholas Scoville Tyler <tylern@lbl.gov>
2022-03-14 18:55:31 +0000

    Removes monitoring in each exec script in favor of !1164

--- test/integration/docker_exec.sh
+++ test/integration/docker_exec.sh
@@ -8,24 +8,9 @@ JOB_SHELL=$6
 SCRIPT=$7
 echo "Executing on $HOSTNAME" 1>&2
 
-# Check if monitoring script is installed
-if [ -x "$(command -v pagurus)" ]; then
-    # Start monitoring running, get monitor pid and sleep
-    pagurus -u $USER -o $PWD/stats.csv &
-    PID=$!
-    sleep 2
-fi
-
 # Run container script and catch exit code
 docker run --rm -it -v $REMOTE_REF:$LOCAL_REF -v $CWD:$DOCKER_CWD --workdir=$DOCKER_CWD --entrypoint=$JOB_SHELL $IMAGE $SCRIPT
 EXIT_CODE=$?
-
-# If PID is set then kill it
-if [ -n "$PID" ]; then
-    # Kill pagurus monitoring and wait for it to write stats.csv
-    kill $PID
-    sleep 2
-fi
 
 # Return with container exit code
 exit $EXIT_CODE
