96cd9b5b35cb6e1331daa0780e7cbf3eb1cb43ca
Seung-Jin Sul <seung-jinsul@PCTechs-MacBook-Pro.local>
2022-04-12 13:39:27 -0700

    Updated test WDLs to disable call-caching

--- condor/cori/cromwell/submit_fqcount.sh
+++ condor/cori/cromwell/submit_fqcount.sh
@@ -1,2 +1,2 @@
-curl -X POST --header "Accept: application/json"    -v "localhost:50011/api/workflows/v1" -F workflowSource=@fq_count.wdl -F workflowInputs=@fq_count.json
+curl -X POST --header "Accept: application/json"    -v "localhost:50011/api/workflows/v1" -F workflowSource=@fq_count.wdl -F workflowInputs=@fq_count.json -F workflowOptions=@options.json
 
--- condor/cori/cromwell/submit_hello.sh
+++ condor/cori/cromwell/submit_hello.sh
@@ -1,1 +1,1 @@
-curl -X POST --header "Accept: application/json"    -v "localhost:50011/api/workflows/v1" -F workflowSource=@hello.wdl -F workflowInputs=@inputs.json
+curl -X POST --header "Accept: application/json"    -v "localhost:50011/api/workflows/v1" -F workflowSource=@hello.wdl -F workflowInputs=@inputs.json -F workflowOptions=@options.json
