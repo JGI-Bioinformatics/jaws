dc50f807737c3079a3ff226113ee969c38e1e543
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2022-04-13 10:51:28 -0700

    Updating idea and simplifying logic
    Starting tests on cori to print queues

--- condor/cori/pool-manager/condor_pool_add.py
+++ condor/cori/pool-manager/condor_pool_add.py
@@ -5,19 +5,31 @@ Condor pool manager for CORI
 
 Author: Seung-Jin Sul (ssul@lbl.gov)
 
-Steps
-1. Collect the IDLE condor job IDs
-2. Get the number of nodes required per memory requirement (normal, jgi_shared, jgi_exvivo)
-   normal: <= 118G
-   jgi_shared: 118 ~ 740G
-   jgi_exvivo: 740 ~ 1450G
-3. Execute sbatch
+# Steps
+# 1. Collect the IDLE condor job IDs
+# 2. Get the number of nodes required per memory requirement (normal, jgi_shared, jgi_exvivo)
+#    normal: <= 118G
+#    jgi_shared: 118 ~ 740G
+#    jgi_exvivo: 740 ~ 1450G
+# 3. Execute sbatch
+
+# Steps
+1. Get idle and running htcondor jobs
+    a. Match running htcondor slot to slurm worker
+    b. See how many nodes can run idle htcondor jobs
+        b.1 ) If no nodes can run task, add to "needed_node_{regular,highmem}" state
+        b.2 ) If nodes are availible, add to "waiting_on_slurm_job" state
+2. Collect current SLURM workers, pending/running
+3. re-run step 1 to see if jobs are draining from htcondor queue
+3. See what is needed and submit sbatch command 
+
 
 """
 import os
-import time 
+import time
 import subprocess
 import shlex
+from typing import Dict
 from utils import run_sh_command
 
 #
@@ -31,14 +43,26 @@ highmem_worker_jgishared_q = f"{condor_root}/condor_worker_highmem_jgi_shared.jo
 highmem_worker_jgiexvivo_q = f"{condor_root}/condor_worker_highmem_jgi_exvivo.job"
 sbatch_cmd = "sbatch --parsable %s"
 sbatch_cmd_esslurm = "module load esslurm && sbatch --parsable %s"
-squeue_cmd = f"""squeue --format="%.18i %.9P %.40j %.8u %.10T %.10M %.9l %.6D %R" --me -u {accnt_name} -t PENDING | grep jaws_condor_normal_worker | wc -l"""
-squeue_cmd_esslurm_shared = f"""module load esslurm && squeue --format="%.18i %.9P %.40j %.8u %.10T %.10M %.9l %.6D %R" --me -u {accnt_name} -t PENDING | grep jaws_condor_highmem_jgi_shared_worker | wc -l"""
-squeue_cmd_esslurm_exvivo = f"""module load esslurm && squeue --format="%.18i %.9P %.40j %.8u %.10T %.10M %.9l %.6D %R" --me -u {accnt_name} -t PENDING | grep jaws_condor_highmem_jgi_exvivo_worker | wc -l"""
 
+
+MIN_POOL = 3
+MAX_POOL = 100
 
 #
 # Execute sbatch
 #
+
+
+def get_current_slurm_workers():
+    # returns csv for with SLURM_ID,PARTITION,NAME,STATE,START-TIME,END-TIME
+    # Checked on
+    squeue_cmd = f"""squeue --clusters=all --format="%.18i %.9P %.40j %.8u %.10T %S %e" -u {accnt_name} | grep {accnt_name} | awk '{{print $1","$2","$3","$5","$6","$7}}' """
+    so, se, ec = run_sh_command(squeue_cmd, show_stdout=False)
+    if ec != 0:
+        print(f"ERROR: failed to execute squeue command: {squeue_cmd}")
+        return None
+
+
 def run_sbatch(sq_cmd: str, idle_jobs: list, batch_script: str, sbatch_cmd: str):
     num_pending_jobs = 0
     so, se, ec = run_sh_command(sq_cmd, show_stdout=False)
@@ -60,68 +84,63 @@ def run_sbatch(sq_cmd: str, idle_jobs: list, batch_script: str, sbatch_cmd: str)
         time.sleep(0.5)
 
 
-#
-# Create condor print format file
-#
-with open(f"{condor_root}/fmt_nobatch_id.cpf", 'w') as cpf:
-    cpf.write("""# condor_q format to list IDLE jobs
-SELECT NOHEADER NOTITLE
-   ClusterId     AS "    ID"  NOSUFFIX WIDTH AUTO
-   RequestMemory AS REQUEST_MEMORY    WIDTH 10    PRINTAS READABLE_MB
-   RequestDisk   AS REQUEST_DISK  WIDTH 12    PRINTAS READABLE_KB
-   RequestCpus   AS REQUEST_CPUS
-WHERE JobStatus == 1""")
+def get_needed_condor_workers() -> Dict:
+    #
+    # Create condor print format file
+    #
+    with open(f"{condor_root}/fmt_nobatch_id.cpf", 'w') as cpf:
+        cpf.write("""# condor_q format to list IDLE jobs
+    SELECT NOHEADER NOTITLE
+    ClusterId     AS "    ID"  NOSUFFIX WIDTH AUTO
+    RequestMemory AS REQUEST_MEMORY    WIDTH 10    PRINTAS READABLE_MB
+    RequestDisk   AS REQUEST_DISK  WIDTH 12    PRINTAS READABLE_KB
+    RequestCpus   AS REQUEST_CPUS
+    WHERE JobStatus == 1""")
+
+    #
+    # Run condor_q_cmd to get the jobs in IDLE status
+    #
+    so, se, ec = run_sh_command(condor_q_cmd, show_stdout=False)
+    if ec != 0:
+        print(f"ERROR: failed to execute condor_q command: {condor_q_cmd}")
+        exit(1)
+
+    print("IDLE Condor jobs")
+    print("Job_id\tReq_mem\tReq_disk\tReq_cpu")
+    print(f"{so.rstrip()}")
+
+    normal_job = []
+    shared_job = []
+    exvivo_job = []
+
+    for l in so.split("\n"):
+        if l and len(shlex.split(l)) == 6:
+            tok = shlex.split(l)
+            job_id = tok[0]
+            req_mem = float(tok[1])
+            mem_unit = tok[2].strip()
+            if mem_unit in ("KB", "kb"):
+                req_mem = req_mem/(1024*1024)
+            if mem_unit in ("MB", "mb"):
+                req_mem = req_mem/1024
+            req_disk = float(tok[3])
+            req_cpu = int(tok[5])
+
+            if req_mem <= 118.0:
+                normal_job.append([job_id, req_mem, req_disk, req_cpu])
+            elif req_mem <= 740.0 and req_cpu <= 18:
+                shared_job.append([job_id, req_mem, req_disk, req_cpu])
+            elif req_mem <= 1450.0 and req_cpu <= 36:
+                exvivo_job.append([job_id, req_mem, req_disk, req_cpu])
+            else:
+                print(
+                    f"ERROR: no machines available for the job id, {job_id}, requested_memory={req_mem}, requested_disk={req_disk}, requested_cpu={req_cpu}"
+                )
+    return {'normal': normal_job, 'exvivo': exvivo_job, 'exvivo_shared': shared_job}
 
 
-#
-# Run condor_q_cmd to get the jobs in IDLE status
-#
-so, se, ec = run_sh_command(condor_q_cmd, show_stdout=False)
-if ec != 0:
-    print(f"ERROR: failed to execute condor_q command: {condor_q_cmd}")
-    exit(1)
-
-print("IDLE Condor jobs")
-print("Job_id\tReq_mem\tReq_disk\tReq_cpu")
-print(f"{so.rstrip()}")
-
-normal_job = []
-shared_job = []
-exvivo_job = []
-
-for l in so.split("\n"):
-    if l and len(shlex.split(l)) == 6:
-        tok = shlex.split(l)
-        job_id = tok[0]
-        req_mem = float(tok[1])
-        mem_unit = tok[2].strip()
-        if mem_unit in ("KB", "kb"):
-            req_mem = req_mem/(1024*1024)
-        if mem_unit in ("MB", "mb"):
-            req_mem = req_mem/1024
-        req_disk = float(tok[3])
-        req_cpu = int(tok[5])
-
-        if req_mem <= 118.0:
-            normal_job.append([job_id, req_mem, req_disk, req_cpu])
-        elif req_mem <= 740.0 and req_cpu <= 18:
-            shared_job.append([job_id, req_mem, req_disk, req_cpu])
-        elif req_mem <= 1450.0 and req_cpu <= 36:
-            exvivo_job.append([job_id, req_mem, req_disk, req_cpu])
-        else:
-            print(
-                f"ERROR: no machines available for the job id, {job_id}, requested_memory={req_mem}, requested_disk={req_disk}, requested_cpu={req_cpu}"
-            )
-
-
-print(f"Normal job: {normal_job}")
-print(f"Jgi_shared job: {shared_job}")
-print(f"Jgi_exvivo job: {exvivo_job}")
-
-
-#
-# Run sbatch
-#
-run_sbatch(squeue_cmd, normal_job, normal_worker_q, sbatch_cmd)
-run_sbatch(squeue_cmd_esslurm_shared, shared_job, highmem_worker_jgishared_q, sbatch_cmd_esslurm)
-run_sbatch(squeue_cmd_esslurm_exvivo, exvivo_job, highmem_worker_jgiexvivo_q, sbatch_cmd_esslurm)
+if __name__ == '__main__':
+    slurm_workers = get_current_slurm_workers()
+    print(slurm_workers)
+    condor_workers = get_needed_condor_workers()
+    print(condor_workers)
