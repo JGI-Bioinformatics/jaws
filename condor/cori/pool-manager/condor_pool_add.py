791da2b2c2fce8dbcf1a464deb5ecf62cd1b1309
Seung-Jin Sul <seung-jinsul@PCTechs-MacBook-Pro.local>
2022-04-14 10:03:04 -0700

    Minor changes

--- condor/cori/pool-manager/condor_pool_add.py
+++ condor/cori/pool-manager/condor_pool_add.py
@@ -95,6 +95,7 @@ def keep_min_pool(sq_cmd: str, batch_script: str, sb_cmd: str):
     num_total_r_pd_jobs = 0
     num_total_r_pd_jobs = run_slurm_cmd(sq_cmd)
     assert num_total_r_pd_jobs != -1
+    print(f"Number of total RUNNING and PENDING slurm jobs: {num_total_r_pd_jobs}")
     if num_total_r_pd_jobs <= min_pool_size:
         to_add = min_pool_size - num_total_r_pd_jobs
         print(f"Add {to_add} nodes to Keep MIN size pool")
