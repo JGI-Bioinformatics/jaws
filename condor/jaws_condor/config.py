38d22f22c47c207379f6cf02d9084d4f9c75367e
Seung-Jin Sul <seung-jinsul@PCTechs-MacBook-Pro.local>
2022-04-19 13:24:49 -0700

    Added config

---
+++ condor/jaws_condor/config.py
@@ -0,0 +1,78 @@
+import logging
+import os
+import configparser
+
+
+conf = None
+
+
+class ConfigurationError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class Configuration():
+    """Configuration singleton class."""
+
+    config = None
+
+    def __init__(self, config_file: str) -> None:
+        """Constructor
+
+        :param config_file: Path to config file in YAML format
+        :type config_file: str
+        """
+        logger = logging.getLogger(__package__)
+        logger.debug(f"Loading configuration from {config_file}")
+        if not os.path.isfile(config_file):
+            raise FileNotFoundError(f"{config_file} does not exist")
+        self.config = configparser.ConfigParser()
+        try:
+            self.config.read(config_file)
+        except Exception as error:
+            logger.exception(f"Unable to load config file {config_file}: {error}")
+            raise
+
+        # save singleton
+        global conf
+        conf = self
+
+    def get_site_id(self):
+        """Returns multiprocessing connection info.
+
+        :return: Multiprocessing parameters.
+        :rtype: dict
+        """
+        section = "SITE"
+        s = self.config[section]
+        params = {
+            "site_id":  s["site_id"],
+            "accnt_name": s["accnt_name"],
+        }
+        return s["site_id"]
+
+    def get_condor_params(self):
+        """Returns Condor pool options.
+
+        :return: Condor pool options.
+        :rtype: dict
+        """
+        section = "CONDOR"
+        s = self.config[section]
+        params = {
+            "condor_root": s["condor_root"],
+            "pool_min_size": s["pool_min_size"],
+            "pool_max_size": s["pool_max_size"],
+        }
+        return params
+
+    def get_slurm_params(self):
+        """Returns site-specific SLURM commands
+
+        :return: Slurm commands
+        :rtype: dict
+        """
+        section = "SLURM"
+        s = self.config[section]
+        params = {}
+        return params
