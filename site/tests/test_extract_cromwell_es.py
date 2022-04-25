d8134c2575650920c38f98c93db2c22bc79c9dfb
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2022-04-25 16:12:33 -0700

    extract_jaws_info returns cromwell id

--- site/tests/test_extract_cromwell_es.py
+++ site/tests/test_extract_cromwell_es.py
@@ -10,30 +10,28 @@ from jaws_site.perf_metrics_es import extract_jaws_info
 jaws-prod/cromwell-executions/rna_count_wrapper/a304bf3c-f042-4af4-8cc5-20bfba245dc9\
 /call-rna_count_DGE/DGE.rna_count_DGE/\
 d04e8fb6-4a37-4f87-8e4e-8a7107523df9/call-diffGeneExp/execution/DGE_files",
-            (
-                "rna_count_wrapper",
-                "a304bf3c-f042-4af4-8cc5-20bfba245dc9",
-                "rna_count_DGE",
-                "DGE_files",
-                "d04e8fb6-4a37-4f87-8e4e-8a7107523df9",
-                "diffGeneExp",
-                0,
-            ),
+            "a304bf3c-f042-4af4-8cc5-20bfba245dc9"
         ),
         (
-            "/var/udiMount/global/cscratch1/sd/jaws_jtm/jaws-prod/\
+            "/global/cscratch1/sd/jaws_jtm/jaws-prod/\
 cromwell-executions/jgi_dap_leo/f92649f2-31f9-4fc4-b1fd-18ed4de94c12\
 /call-trimAlign_expt/shard-37/execution",
-            (
-                "jgi_dap_leo",
-                "f92649f2-31f9-4fc4-b1fd-18ed4de94c12",
-                "trimAlign_expt",
-                "nosub",
-                "nosub",
-                "nosub",
-                37,
-            ),
+            "f92649f2-31f9-4fc4-b1fd-18ed4de94c12"
         ),
+        (
+            "cromwell-executions/\
+                fq_count/8b1669c3-9bca-4047-99b4-e3556b71c4f4/\
+                    call-count_seqs/execution",
+            "8b1669c3-9bca-4047-99b4-e3556b71c4f4"
+        ),
+        (
+            "/global/cfs/cdirs/jaws/jaws-install/jaws-supervisord-prod",
+            "naw"
+        ),
+        (
+            "None",
+            "naw"
+        )
     ],
 )
 def test_extract_cromwell_es(working_dir, expect):
