7e89f394d7b3bc8709915f2547077887c508538c
Steve Chan <sychan168@gmail.com>
2022-05-18 11:40:04 -0700

    Misread the class structure! get() is a method in ConfigParser.

--- central/jaws_central/env_interpolation.py
+++ central/jaws_central/env_interpolation.py
@@ -11,6 +11,17 @@ class EnvInterpolation(configparser.BasicInterpolation):
     Cribbed shamelessly from https://gist.github.com/malexer/ee2f93b1973120925e8beb3f36b184b8
     Uses normal interpolation syntax for configparser
     https://docs.python.org/3/library/configparser.html#interpolation-of-values
+    """
+
+    def before_get(self, parser, section, option, value, defaults):
+        value = super().before_get(parser, section, option, value, defaults)
+        return os.path.expandvars(value)
+
+
+class JAWSConfigParser(configparser.ConfigParser):
+    """
+    Subclass of the normal configparser that allows config options to be totally overriden by an
+    environment variable.
 
     A new parameter is introduced to the init() function, env_override, which is a prefix for
     any environment variables that should be used as overrides for the get() function. Must be
@@ -31,14 +42,11 @@ class EnvInterpolation(configparser.BasicInterpolation):
             self._vars = None
         configparser.BasicInterpolation.__init__(self, **kwargs)
 
-    def before_get(self, parser, section, option, value, defaults):
-        value = super().before_get(parser, section, option, value, defaults)
-        return os.path.expandvars(value)
-
     def get(self, section, option, **kwargs):
         """
         Override the standard getter by forcing the vars argument to use the dictionary derived
         from the environment variables with the prefix in the env_override param to init
         """
         value = super().get(self, section, option, vars=self._vars, **kwargs)
+        print("section: ", section, " option:", option, " vars: ", self._vars)
         return(value)
