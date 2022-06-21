5b017b370166048e95e64fa6ace64e4406a7dd11
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2022-06-21 14:47:02 -0700

    Add new tests to test bugs

--- site/tests/test_perf_metrics.py
+++ site/tests/test_perf_metrics.py
@@ -49,6 +49,14 @@ def test_parse_perf_metrics_task_dir():
             "/global/cscratch1/sd/jaws_jtm/jaws-dev/cromwell-executions/main_workflow/e7f02164-2d3d-4cfb-828a-f3da23c43280/call-hello_and_goodbye_1/sub.hello_and_goodbye/3327f701-769a-49fe-b407-eb4be3a4a373/call-hello/execution",  # noqa
             "main_workflow.hello_and_goodbye_1:hello_and_goodbye.hello",
         ],
+        [
+            "cromwell-executions/main_align_wdl/8d357504-8eda-4d1c-b3c4-f87a0bdd29f8/call-bbmap_shard_wf/align.bbmap_shard_wf/27536cf7-4b8f-486c-9085-872625afaef1/call-bbmap_indexing/execution",  # noqa
+            "main_align_wdl.bbmap_shard_wf:bbmap_shard_wf.bbmap_indexing",
+        ],
+        [
+            "cromwell-executions/main_align_wdl/8d357504-8eda-4d1c-b3c4-f87a0bdd29f8/call-bbmap_shard_wf/align.bbmap_shard_wf/27536cf7-4b8f-486c-9085-872625afaef1/call-alignment/shard-0/execution",  # noqa
+            "main_align_wdl.bbmap_shard_wf:bbmap_shard_wf.alignment[0]"
+        ]
     ]
 
     for task_dir, expected in test_data:
