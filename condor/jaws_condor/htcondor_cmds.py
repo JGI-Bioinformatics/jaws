8ff66f1dbe21b2b0a735fa4b90a9e6f9893efdaf
Nicholas Scoville Tyler <tylern@lbl.gov>
2022-08-24 19:26:53 +0000

    Resolve "Create Pool manager Daemon"

---
+++ condor/jaws_condor/htcondor_cmds.py
@@ -0,0 +1,105 @@
+# from jaws_condor import config
+from typing import Dict, List
+from jaws_condor.cmd_utils import run_sh_command
+import logging
+import pandas as pd
+import time
+
+logger = logging.getLogger(__package__)
+
+
+class HTCondorError(Exception):
+    pass
+
+
+class HTCondorCmdFailed(HTCondorError):
+    pass
+
+
+class HTCondor:
+    def __init__(self, columns, **kwargs):
+        self.columns = columns
+
+    def condor_q(self):
+        # Call the command
+        condor_q_cmd = f"condor_q -allusers -af {self.columns}"
+        try:
+            condor_jobs = self.call_condor_command(condor_q_cmd)
+            # get serialized and processed dataframe
+            dataframe = process_condor_q(condor_jobs, self.condor_columns())
+        except HTCondorCmdFailed as err:
+            logging.error(
+                f"{type(err).__name__} make sure condor is running and CONDOR_CONFIG is set properly"
+            )
+            dataframe = process_condor_q([], self.condor_columns())
+
+        return dataframe
+
+    def condor_idle(self):
+        get_idle = 'condor_status -const "TotalSlots == 1" -af Machine'
+        try:
+            idle_list = self.call_condor_command(get_idle)
+            # Takes a list of lists to a single list
+            idle = [item for sublist in idle_list for item in sublist]
+        except HTCondorCmdFailed as err:
+            logging.error(
+                f"{type(err).__name__} make sure condor is running and CONDOR_CONFIG is set properly"
+            )
+            idle = []
+        return idle
+
+    def condor_columns(self):
+        # Splits the string columns to be used as header of dataframe
+        return self.columns.split()
+
+    def call_condor_command(self, condor_cmd: str) -> List:
+        # Call the condor_q with the desired options
+        stdout, stderr, returncode = run_sh_command(
+            condor_cmd, log=logger, show_stdout=False
+        )
+        # The usual failures
+        if returncode != 0:
+            logger.critical(f"ERROR: failed to execute condor_q command: {condor_cmd}")
+            raise HTCondorCmdFailed(f"{condor_cmd} command failed with {stderr}")
+
+        return parse_condor_outputs(stdout)
+
+
+def parse_condor_outputs(stdout: str) -> List:
+    # split outputs by rows
+    outputs = stdout.split("\n")
+    # Split each row into columns
+    condor_jobs = [job.split() for job in outputs]
+    # Removes columns with no values (Usually the last column)
+    condor_jobs = [q for q in condor_jobs if len(q) != 0]
+    return condor_jobs
+
+
+def process_condor_q(condor_jobs: List, columns: List) -> List[Dict]:
+    # if there are no jobs send back empty array of jobs
+    if len(condor_jobs) == 0:
+        return [{k: 0 for k in columns}]
+    # Create a dataframe from the split outputs
+    df = pd.DataFrame(condor_jobs, columns=columns)
+    # Change the types
+    df["JobStatus"] = df["JobStatus"].astype(int)
+    df["RequestMemory"] = df["RequestMemory"].astype(float) / 1024
+    df["RequestCpus"] = df["RequestCpus"].astype(float)
+    df["CumulativeRemoteSysCpu"] = df["CumulativeRemoteSysCpu"].astype(float)
+    df["CumulativeRemoteUserCpu"] = df["CumulativeRemoteUserCpu"].astype(float)
+
+    now = int(time.time())
+    df["JobStartDate"] = df["JobStartDate"].str.replace("undefined", str(now))
+    df["total_running_time"] = now - df["JobStartDate"].astype(int)
+    df["cpu_percentage"] = (
+        (
+            (df["CumulativeRemoteSysCpu"] + df["CumulativeRemoteUserCpu"])
+            / df["RequestCpus"]
+        )
+        / df["total_running_time"]
+    ) * 100
+    df["total_q_time"] = df["JobStartDate"].astype(int) - df["QDate"].astype(int)
+
+    # Returns a serilized/json version better for passing between objects
+    # And for testing
+    return df.to_dict("records")
---
+++ condor/jaws_condor/slurm_cmds.py
@@ -0,0 +1,148 @@
+from jaws_condor.cmd_utils import run_sh_command
+import logging
+import pandas as pd
+
+
+logger = logging.getLogger(__package__)
+
+
+class SlurmError(Exception):
+    pass
+
+
+class SlurmCmdFailed(SlurmError):
+    pass
+
+
+class Slurm:
+    def __init__(
+        self,
+        user_name: str = "jaws_jtm",
+        extra_args: str = None,
+        script_path: str = "",
+        **kwargs,
+    ):
+        self.squeue_columns = [
+            "JOBID",
+            "PARTITION",
+            "NAME",
+            "STATE",
+            "TIME_LEFT",
+            "NODELIST",
+        ]
+        self.squeue_cmd = (
+            'squeue --format="%.12i %.10P %.50j %.10T %.10L %.12R" --noheader'
+        )
+        self.user_name = user_name
+        self.extra_args = extra_args
+        self.script_path = script_path
+
+    @property
+    def columns(self) -> list:
+        """
+        Returns the cloumns gotten from the 'squeue' command
+        """
+        return self.squeue_columns
+
+    def call_slurm_command(self, command):
+        """
+        Genereic function to call a command and raise an error
+        """
+        stdout, stderr, returncode = run_sh_command(
+            command, log=logger, show_stdout=False
+        )
+
+        # If we have an error return a dictionary with 0 for each type and state
+        if returncode != 0:
+            logger.error(f"Bad Return code from: {command}")
+            raise SlurmCmdFailed(f"'{command}' command failed with {stderr}")
+
+        return stdout, stderr, returncode
+
+    def squeue(self) -> dict:
+        """
+        Runs the squeue command and returns a dictionary
+        compatible with a pandas dataframe
+        """
+        command = f"{self.squeue_cmd}  -u {self.user_name} {str(self.extra_args)}"
+        try:
+            stdout, stderr, returncode = self.call_slurm_command(command)
+        except SlurmCmdFailed:
+            logging.error("squeue failed")
+            stdout = ""
+
+        # Create an empty dataframe with the right columns if nothing was returned or an error
+        if stdout == "":
+            return pd.DataFrame([], columns=[*self.columns, "TIME_SEC"]).to_dict()
+
+        # Gets jobs from output by splitting on new lines
+        jobs = [job.split() for job in stdout.split("\n")]
+        try:
+            df = pd.DataFrame(jobs, columns=self.columns)
+        except AssertionError:
+            logging.error(f'{jobs} mismatch {self.columns}')
+            return pd.DataFrame([], columns=[*self.columns, 'TIME_SEC']).to_dict()
+        # Drops rows if they have nan values
+        df = df.dropna(axis=0)
+
+        # Adds a new column of time in seconds left to run
+        df["TIME_SEC"] = df["TIME_LEFT"].apply(slurm_time_to_sec)
+
+        return df.to_dict()
+
+    def scancel(self, job_id: int = 0, cluster: str = ""):
+
+        cluster = "" if cluster is None else cluster
+        if cluster != "":
+            cluster = f"-M {cluster}"
+
+        command = f"scancel {cluster} {job_id}"
+        try:
+            stdout, stderr, returncode = self.call_slurm_command(command)
+            return {"stdout": stdout, "stderr": stderr, "returncode": returncode}
+        except SlurmCmdFailed:
+            logging.error("scancel failed")
+            return {"stdout": "failed", "stderr": "failed", "returncode": 1}
+
+    def sbatch(self, compute_type: str = "medium", cluster: str = ""):
+
+        cluster = "" if cluster is None else cluster
+        if cluster != "":
+            cluster = f"-M {cluster}"
+
+        command = f"sbatch --parsable {cluster} {self.script_path}/condor_worker_{compute_type}.job"
+        try:
+            stdout, stderr, returncode = self.call_slurm_command(command)
+            return {"stdout": stdout, "stderr": stderr, "returncode": returncode}
+        except SlurmCmdFailed:
+            logging.error("sbatch failed")
+            return {"stdout": "failed", "stderr": "failed", "returncode": 1}
+
+
+def slurm_time_to_sec(time_str):
+    # split off days first
+    time_str = time_str.split("-")
+    if len(time_str) > 1:
+        days = int(time_str[0])
+    else:
+        days = 0
+
+    # split time_str into HH:MM:SS
+    time_str = time_str[-1].split(":")
+
+    time_str_bits = {0: 1, 1: 60, 2: 60 * 60, 3: 60 * 60 * 24}
+    total = 0
+    # Run in reverse becasue we will always
+    # have seconds and not always hours
+    # 0 -> sec
+    # 1 -> min
+    # 2 -> hrs.
+    # 3 -> days.
+    for i, t in enumerate(time_str[::-1]):
+        total += time_str_bits[i] * int(t)
+
+    if days > 0:
+        total += time_str_bits[3] * int(days)
+
+    # Return total seconds
+    return total
