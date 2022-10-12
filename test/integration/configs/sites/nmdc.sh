d394c9a98e1b1bb95338187a683922479e99f873
Edward Steven Kirton <eskirton@lbl.gov>
2022-10-12 11:59:29 -0700

    change install dir for nmdc

--- test/integration/configs/sites/nmdc.sh
+++ test/integration/configs/sites/nmdc.sh
@@ -1,4 +1,4 @@
-export JAWS_INSTALL_BASEDIR="/global/common/software/m3408"
+export JAWS_INSTALL_BASEDIR="/global/common/software/m3408/jaws-install"
 export JAWS_GLOBUS_EP="be1ff650-dcbc-11ea-85a2-0e1702b77d41"
 export JAWS_GLOBUS_HOST_PATH="/"
 export JAWS_LOAD_PYTHON="module load python/3.9-anaconda-2021.11"
