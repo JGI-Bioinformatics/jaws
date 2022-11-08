fdcf0c5c4075e5e9268ee70d52e61a51f94348b4
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2022-11-07 16:28:20 -0800

    Move pagurus to site

---
+++ site/tests/test_pagurus.py
@@ -0,0 +1,114 @@
+import pytest
+import json
+import os
+from jaws_site import pagurus
+
+
+def test_FileWriter_csv(tmp_path):
+    print("Output file path", tmp_path)
+    header = ['name1', "name2", "name3"]
+    outfile = tmp_path/"test.csv"
+    fw = pagurus.FileWriter(outfile=outfile, header=header, write_header=True)
+    assert(fw.header == header)
+    assert(fw.write_header is True)
+    assert(fw.outfile == outfile)
+    assert(outfile.is_file())
+    fw.write(0, 1, 2)
+    fw.close()
+    with open(outfile) as f:
+        contents = f.read()
+        print(contents)
+        lines = contents.splitlines()
+        assert(lines[0] == ",".join(header))
+        assert(lines[1] == "0,1,2")
+    fw.outfile.unlink()
+
+
+def test_FileWriter_csv_envvar(tmp_path):
+    print("Output file path", tmp_path)
+    header = ['name1', "name2", "name3"]
+    outfile = tmp_path/"test.csv"
+
+    # Clear out environment variables we will be testing with
+    try:
+        os.environ.pop("testytesty")
+        os.environ.pop("testytoasty")
+    except Exception as e:
+        print(f'Error ({type(e).__name__}): {e}')
+        pass
+    os.environ['testytesty'] = 'test'
+    os.environ['testytoasty'] = 'test2'
+    fw = pagurus.FileWriter(outfile=outfile, header=header, write_header=True, env=["testytesty", "testytoasty"])
+    header2 = header+["testytesty", "testytoasty"]
+
+    assert(fw.header == header2)
+    assert(fw.write_header is True)
+    assert(fw.outfile == outfile)
+    assert(outfile.is_file())
+    fw.write(0, 1, 2, "test", "test2")
+    fw.close()
+    with open(outfile) as f:
+        contents = f.read()
+        print(contents)
+        lines = contents.splitlines()
+        assert(lines[0] == ",".join(header2))
+        assert(lines[1] == '0,1,2,test,test2')
+    fw.outfile.unlink()
+
+
+def test_FileWriter_json(tmp_path):
+    print("Output file path", tmp_path)
+    header = ['name1', "name2", "name3"]
+    outfile = tmp_path/"test.json"
+    fw = pagurus.FileWriter(outfile=outfile, header=header, write_header=True, jsonout=True)
+    assert(fw.header == header)
+    assert(fw.write_header is False)
+    assert(fw.outfile == outfile)
+    assert(outfile.is_file())
+    fw.write(0, 1, 2)
+    fw.close()
+    with open(outfile) as f:
+        contents = f.read()
+        print(contents)
+        lines = contents.splitlines()
+        jsonout = json.dumps(dict(zip(header, [0, 1, 2])))
+        assert(lines[0] == jsonout)
+    fw.outfile.unlink()
+
+
+def test_FileWriter_json_envvar(tmp_path):
+    print("Output file path", tmp_path)
+    header = ['name1', "name2", "name3"]
+    outfile = tmp_path/"test.json"
+
+    # Clear out environment variables we will be testing with
+    try:
+        os.environ.pop("testytesty")
+        os.environ.pop("testytoasty")
+    except Exception as e:
+        print(f'Error ({type(e).__name__}): {e}')
+        pass
+    with pytest.raises(KeyError) as e:
+        print(f'Error ({type(e).__name__}): {e}')
+        pass
+        fw = pagurus.FileWriter(outfile=outfile, header=header, write_header=True,
+                                jsonout=True, env=["testytesty", "testytoasty"])
+    os.environ['testytesty'] = 'test'
+    os.environ['testytoasty'] = 'test2'
+    fw = pagurus.FileWriter(outfile=outfile, header=header, write_header=True,
+                            jsonout=True, env=["testytesty", "testytoasty"])
+    header2 = header+["testytesty", "testytoasty"]
+
+    assert(fw.header == header2)
+    assert(fw.write_header is False)
+    assert(fw.outfile == outfile)
+    assert(outfile.is_file())
+    fw.write(0, 1, 2, "test", "test2")
+    fw.close()
+    with open(outfile) as f:
+        contents = f.read()
+        print(contents)
+        lines = contents.splitlines()
+        jsonout = json.dumps(dict(zip(header2, [0, 1, 2, "test", "test2"])))
+        assert(lines[0] == jsonout)
+    fw.outfile.unlink()
