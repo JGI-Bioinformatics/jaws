b91d9d180e5112f76afddeb00f807225848541a8
Edward Steven Kirton <eskirton@lbl.gov>
2022-12-13 10:55:12 -0800

    add JAWS_RPC_SERVER_NUM_THREADS to site config, set to 16 for prod

--- test/integration/configs/sites/assembly.sh
+++ test/integration/configs/sites/assembly.sh
@@ -1,8 +1,8 @@
-export JAWS_INSTALL_BASEDIR="/home/ec2-user"
+export JAWS_ACCESS_GROUP="assembly"
 export JAWS_GLOBUS_EP=""
 export JAWS_GLOBUS_HOST_PATH=""
 export JAWS_GROUP="ec2-user"
-export JAWS_USERS_GROUP="ec2-user"
-export JAWS_SCRATCH_BASEDIR="s3://jaws-assembly"
+export JAWS_INSTALL_BASEDIR="/home/ec2-user"
 export JAWS_MAX_RAM_GB=512
-export JAWS_ACCESS_GROUP="assembly"
+export JAWS_SCRATCH_BASEDIR="s3://jaws-assembly"
+export JAWS_USERS_GROUP="ec2-user"
