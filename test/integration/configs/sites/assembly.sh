f4c34085bd9d4ec2d65d8098d4b78b8945d86b14
Jeff Froula <jlfroula@lbl.gov>
2023-01-30 15:31:45 -0800

    add max cpu values to config

--- test/integration/configs/sites/assembly.sh
+++ test/integration/configs/sites/assembly.sh
@@ -4,5 +4,6 @@ export JAWS_GLOBUS_HOST_PATH=""
 export JAWS_GROUP="ec2-user"
 export JAWS_USERS_GROUP="ec2-user"
 export JAWS_SCRATCH_BASEDIR="s3://jaws-assembly"
-export JAWS_MAX_RAM_GB=512
+export JAWS_MAX_RAM_GB=502
+export JAWS_MAX_CPU=128
 export JAWS_ACCESS_GROUP="assembly"
