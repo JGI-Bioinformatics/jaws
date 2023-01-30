70566ee1371bcb94d031e2ee27a50857d50960a7
Seung-Jin Sul <ssul@lbl.gov>
2023-01-30 15:53:00 -0800

    Added dns setting

--- test/integration/templates/jaws-site.sh
+++ test/integration/templates/jaws-site.sh
@@ -10,25 +10,56 @@ JAWS_GITLAB_RUNNER_CONFIG="$JAWS_GITLAB_RUNNER_CONFIG"
 LOGFILE="$JAWS_LOGS_DIR/jaws-site.log"
 
 
+# Dynamic dns setting
+HOSTIP=`hostname -i`
+SITEDNSNAME="${JAWS_SITE_DNS_NAME}"
+PSCRATCHDNSNAME="pscratch-s3.jaws.dyn.jgi.doe.gov"
+
+# The last Minio release that allows the creation of new NAS gateways was docker pull 2022-05-26T05-48-41Z
+# The last release that allowed you to use the NAS gateway on an existing setup was 2022-10-24T18-35-07Z
+# Export these so that Minio sees them
+export MINIO_ROOT_USER=jaws
+export MINIO_ROOT_PASSWORD=thisisthejawspassword.
+export MINIO_BROWSER=off
+
+MINIO_EXE="/global/u2/j/jaws/minio/minio"
+MINIOPORT=9000
+LISTEN="${DOLLAR}{PSCRATCHDNSNAME}:${DOLLAR}{MINIOPORT}"
+
+# See if we have a HTTP listener on the existing DNSNAME port 9000, if so then bail since that
+# needs to be shutdown first
+curl http://${DOLLAR}{LISTEN}/
+if [ ${DOLLAR}? -eq 0 ]; then
+    echo "Existing listener at ${DOLLAR}{LISTEN}. It must be shutdown before we start another"
+    pkill -u jaws minio
+fi
+
+echo "Setting DNS for ${DOLLAR}SITEDNSNAME with ${DOLLAR}HOSTIP"
+TSIGPATH='/global/u2/j/jaws/.ssh/jaws_TSIG_key'
+cat <<EOF >/tmp/JAWS_SITEDNS.${DOLLAR}${DOLLAR}
+server nsx.lbl.gov
+zone dyn.jgi.doe.gov
+update del ${DOLLAR}SITEDNSNAME A
+update add ${DOLLAR}SITEDNSNAME 30 A ${DOLLAR}HOSTIP
+update del ${DOLLAR}PSCRATCHDNSNAME A
+update add ${DOLLAR}PSCRATCHDNSNAME 120 A ${DOLLAR}HOSTIP
+send
+EOF
+
+nsupdate -v -k ${DOLLAR}{TSIGPATH} </tmp/JAWS_SITEDNS.${DOLLAR}${DOLLAR}
+if [ ${DOLLAR}? -eq 0 ]; then
+    sleep 30
+    echo "Successfully set DNS entry"
+    dig +short ${DOLLAR}SITEDNSNAME
+    dig +short ${DOLLAR}PSCRATCHDNSNAME
+    #${DOLLAR}MINIO_EXE gateway nas --address ${DOLLAR}HOSTIP:${DOLLAR}MINIOPORT /pscratch/sd/j/jaws
+else
+    echo "Failed to set DNS entry. Exiting"
+    exit 1
+fi
+
+
 echo "Starting jaws-site on `hostname -i`" > "${DOLLAR}LOGFILE"
-
-
-# shutdown cleanly
-function stopall {
-    echo "Stopping services" > "${DOLLAR}LOGFILE"
-    "${DOLLAR}JAWS_BIN_DIR/supervisorctl" stop all 2>&1 >> "${DOLLAR}LOGFILE"
-    echo "Shutdown supervisor" >> "${DOLLAR}LOGFILE"
-    "${DOLLAR}JAWS_BIN_DIR/supervisorctl" shutdown 2>&1 >> "${DOLLAR}LOGFILE"
-    echo "Stopping gitlab-runner" >> "${DOLLAR}LOGFILE"
-    "${DOLLAR}JAWS_GITLAB_RUNNER" stop -c "${DOLLAR}JAWS_GITLAB_RUNNER_CONFIG" 2>&1 >> "${DOLLAR}LOGFILE"
-    echo "Done" >> "${DOLLAR}LOGFILE"
-}
-trap stopall EXIT
-
-
-# Start gitlab-runner
-# This is not part of the CI/CD and must be setup manually first.
-"${DOLLAR}JAWS_GITLAB_RUNNER" run -c "${DOLLAR}JAWS_GITLAB_RUNNER_CONFIG"
 
 # Start supervisord
 "${DOLLAR}JAWS_BIN_DIR/supervisord"
