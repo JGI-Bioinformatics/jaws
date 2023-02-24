87e7395b1d302454df32473b97ab3c71e32bf2ec
Edward Steven Kirton <eskirton@lbl.gov>
2023-02-24 14:20:02 -0800

    change aws ci/cd user, group, install path

--- test/integration/configs/sites/assembly.sh
+++ test/integration/configs/sites/assembly.sh
@@ -1,8 +1,8 @@
-export JAWS_INSTALL_BASEDIR="/home/ec2-user/jaws-install"
+export JAWS_INSTALL_BASEDIR="/etc/jaws"
 export JAWS_GLOBUS_EP=""
 export JAWS_GLOBUS_HOST_PATH=""
-export JAWS_GROUP="ec2-user"
-export JAWS_USERS_GROUP="ec2-user"
+export JAWS_GROUP="jaws"
+export JAWS_USERS_GROUP="jaws"
 export JAWS_SCRATCH_BASEDIR="s3://jaws-assembly-${JAWS_DEPLOYMENT_NAME}"
 export JAWS_REF_DATA_DIR=""
 export JAWS_MAX_RAM_GB=502
