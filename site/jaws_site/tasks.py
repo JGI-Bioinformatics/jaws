dac8f847b85b244271c51a71e23cfa452847f3ca
Edward Steven Kirton <eskirton@lbl.gov>
2023-09-27 01:53:44 -0700

    test

--- site/jaws_site/tasks.py
+++ site/jaws_site/tasks.py
@@ -314,12 +314,12 @@ class TaskLog:
                 status = "cancelled"
             update = {
                 "id": row_id,
-                # "cached": bool(summary[task_dir]["cached"]),
+                "status": status,
+                "cached": bool(summary[task_dir]["cached"]),
                 # "name": summary[task_dir]["name"],
                 # "req_cpu": int(summary[task_dir]["requested_cpu"]),
                 # "req_mem_gb": self.memory_gb(summary[task_dir]["requested_memory"]),
                 # "req_minutes": self.time_minutes(summary[task_dir]["requested_time"]),
-                "status": status,
             }
             updates.append(update)
         return updates
