df3d2fd5ef2f3978a45c732afbc1b3eff58fe0ba
Daniela Cassol <dcassol@lbl.gov>
2024-08-21 15:51:26 +0000

    Update 2 files
    - /test/integration/templates/supervisorctl.sh
    - /test/integration/templates/supervisord.sh

--- test/integration/templates/supervisorctl.sh
+++ test/integration/templates/supervisorctl.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -l
 
+$JAWS_LOAD_PYTHON
 source "$JAWS_SUPERVISOR_DIR/bin/activate"
 exec supervisorctl -c "$JAWS_CONFIG_DIR/supervisor.conf" $@
--- test/integration/templates/supervisord.sh
+++ test/integration/templates/supervisord.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -l
 
+$JAWS_LOAD_PYTHON
 source "$JAWS_SUPERVISOR_DIR/bin/activate"
 exec supervisord -c "$JAWS_CONFIG_DIR/supervisor.conf" $@
