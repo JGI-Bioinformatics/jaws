e887ddd9dc2dec50eec4881aa1714f06117e6763
Seung-Jin Sul <ssul@lbl.gov>
2024-09-05 14:14:46 -0700

    Removed unused perf metrics files and vars

--- test/integration/templates/all.sh
+++ test/integration/templates/all.sh
@@ -17,7 +17,6 @@ fi
 # Folders for Run inputs and downloads from other jaws-sites
 export JAWS_INPUTS_DIR="$JAWS_SCRATCH_DIR/inputs"
 export JAWS_DOWNLOADS_DIR="$JAWS_SCRATCH_DIR/downloads"
-export JAWS_PERFORMANCE_METRICS_DIR="${JAWS_SCRATCH_DIR}/monitoring-runs"
 
 # Cromwell-related dirs
 export JAWS_CROMWELL_EXECUTIONS_DIR="$JAWS_SCRATCH_DIR/cromwell-executions"
