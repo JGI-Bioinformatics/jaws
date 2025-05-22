146280ab5761398bc6e833fba6d0a9ca45b44ad7
Mario Melara <maamelara@gmail.com>
2025-05-22 12:40:47 -0700

    chore: only use auth if provided

--- src/jaws_site/cromwell.py
+++ src/jaws_site/cromwell.py
@@ -59,7 +59,10 @@ retries = urllib3.Retry(
 
 session.mount("http://", requests.adapters.HTTPAdapter(max_retries=retries))
 params = config.conf.get_section("CROMWELL")
-session.auth = (params.get("user"), params.get("password"))
+
+## Set up authentication if provided
+if params.get("user") is not None and params.get("password") is not None:
+    session.auth = (params.get("user"), params.get("password"))
 
 time_re = re.compile(r"^\s*(\d+):(\d+):(\d+)\s*$")
 memory_re = re.compile(r"^\s*(\d+)\s*(\w+)\s*$")
